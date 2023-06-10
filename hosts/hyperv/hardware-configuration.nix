b892b65f513ff5f65c6cd7c4d79f24a0a3e89499
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2023-06-10 16:16:23 +0200

    initial hyperv host

---
+++ hosts/hyperv/hardware-configuration.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ ];
+
+  boot.initrd.availableKernelModules = [ "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixroot";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/nixboot";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ 
+    {
+      device = "/.swapfile";
+      size = 4 * 1024;
+    }
+  ];
+
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  virtualisation.hypervGuest.enable = true;
+}
