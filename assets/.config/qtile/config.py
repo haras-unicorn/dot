fe05bfdfc09680050e8db8e1aa472b0a3207a9af
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2023-06-14 02:45:07 +0200

    WIP

--- assets/.config/qtile/config.py
+++ assets/.config/qtile/config.py
@@ -820,9 +820,9 @@ for group_name in visible_group_names:
 # Hooks
 
 
-@hook.subscribe.startup_once
-def startup_once():
-    lazy.spawn("betterlockscreen --update " + lock_wallpaper_loc)
+# @hook.subscribe.startup_once
+# def startup_once():
+#     lazy.spawn("betterlockscreen --update " + lock_wallpaper_loc)
 
 
 @hook.subscribe.client_new
