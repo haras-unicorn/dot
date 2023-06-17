d63c83873b0b29928486fe36641aa0c190284b85
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2023-06-18 01:25:14 +0200

    WIP

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,9 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-  nixpkgs.allowUnfree = true;
+  nixpkgs.allowUnfree = {
+    allowUnfree = true;
+  };
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.loader.efi.canTouchEfiVariables = true;
