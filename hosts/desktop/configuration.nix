b846e4de4afd804ca0a9ab9849b45cb58b7f2594
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2023-06-17 17:50:36 +0200

    WIP

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -17,12 +17,12 @@
   boot.initrd.systemd.enable = true;
   boot.kernelParams = [
     "quiet"
-    # "splash"
-    # "vt.global_cursor_default=0"
-    # "vga=current"
-    # "loglevel=3"
-    # "rd.systemd.show_status=auto"
-    # "rd.udev.log_level=3"
+    "splash"
+    "vt.global_cursor_default=0"
+    "vga=current"
+    "loglevel=3"
+    "systemd.show_status=auto"
+    "rd.udev.log_level=3"
   ];
   boot.plymouth.enable = true;
   boot.plymouth.theme = "nixos-bgrt";
