32cdb294ba2f28c01505b52a5ad3dee5dd1b53b9
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2023-06-18 16:55:13 +0200

    WIP

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -55,14 +55,18 @@
   networking.networkmanager.enable = true;
 
   services.picom.enable = true;
+  programs.dconf.enable = true;
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.libinput.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.autoNumlock = true;
   services.xserver.displayManager.sddm.theme = "${sweet-theme}/kde/sddm";
-  programs.dconf.enable = true;
-  # TODO: remove and switch to another wm (i3 cuz easy or leftwm cuz rust)
+  services.xserver.displayManager.defaultSession = "xfce";
+  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.xfce.noDesktop = true;
+  services.xserver.desktopManager.xfce.enableScreensaver = false;
+  services.xserver.desktopManager.xfce.enableXfwm = false;
   services.xserver.windowManager.qtile.enable = true;
   services.xserver.windowManager.qtile.extraPackages =
     python3Packages: with python3Packages; [
