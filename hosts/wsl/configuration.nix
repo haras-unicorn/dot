1af69751ef68e3487e294f51f065de88de645296
Hrvoje Juric <social@hrvojej.anonaddy.me>
2023-08-18 16:54:11 +0000

    WIP

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,29 +1,30 @@
 { pkgs
-, nixos-wsl 
-, ... 
+, nixos-wsl
+, ...
 }:
 
 {
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = "experimental-features = nix-command flakes";
+  nixpkgs.config = import ../../assets/.config/nixpkgs/config.nix;
+
   imports = [
     nixos-wsl.nixosModules.wsl
   ];
 
-  wsl = {
-    enable = true;
-    wslConf.automount.root = "/mnt";
-    defaultUser = "nixos";
-    startMenuLaunchers = true;
-  };
-
-  nix.package = pkgs.nixFlakes;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
+  wsl.enable = true;
+  wsl.startMenuLaunchers = true;
+  wsl.defaultUser = "nixos";
 
   environment.systemPackages = with pkgs; [
     vim
     git
+    man-pages
+    man-pages-posix
+    nushell
   ];
+
+  users.users.nixos.shell = pkgs.nushell;
 
   system.stateVersion = "23.11";
 }
