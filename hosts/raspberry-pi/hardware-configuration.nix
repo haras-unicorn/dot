398c7219f16a62391ebcc70cc62da66a00a73271
Hrvoje Juric <social@hrvojej.anonaddy.me>
2023-08-19 15:28:10 +0000

    WIP

--- hosts/raspberry-pi/hardware-configuration.nix
+++ hosts/raspberry-pi/hardware-configuration.nix
@@ -1,8 +1,8 @@
-{ nixpkgs, nixos-hardware, ... }:
+{ modulesPath, nixos-hardware, ... }:
 
 {
   imports = [
     nixos-hardware.nixosModules.raspberry-pi-4
-    "<nixpkgs>/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
 }
