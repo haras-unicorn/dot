4a7032260bd1ffe1423cef8c969481cb9d9f0ee7
Hrvoje Juric <social@hrvojej.anonaddy.me>
2023-08-19 16:39:45 +0000

    WIP

--- hosts/raspberry-pi/hardware-configuration.nix
+++ hosts/raspberry-pi/hardware-configuration.nix
@@ -4,4 +4,12 @@
   imports = [
     nixos-hardware.nixosModules.raspberry-pi-4
   ];
+
+  # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1008362877  
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
 }
