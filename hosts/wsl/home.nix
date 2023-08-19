06e2e9ca58e1f410a4d17dea2080949b92ff3efe
Hrvoje Juric <social@hrvojej.anonaddy.me>
2023-08-19 10:24:13 +0000

    WIP

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -114,93 +114,117 @@ in
   programs.helix.themes = {
     palenight =
       let
-        # transparent = "#000000";
-        background = "#191349";
-        foreground = "#c7d5ff";
-        black = "#132339";
-        white = "#ffddff";
-        blue = "#82aaff";
-        cyan = "#89ddff";
-        green = "#c3e88d";
-        magenta = "#c792ea";
-        # red = "#ff5874";
-        yellow = "#ffeb95";
-        brightBlack = "#3c435e";
-        brightWhite = "#ffffff";
-        brightBlue = "#92baff";
-        brightCyan = "#99fdff";
-        brightGreen = "#c3f88d";
-        brightMagenta = "#d792fa";
-        brightRed = "#ff6884";
-        brightYellow = "#fffba5";
-        dimBlack = "#000200";
-        dimWhite = "#ddccdd";
-        dimBlue = "#72baff";
-        # dimCyan = "#79edff";
-        # dimGreen = "#b3d87d";
-        # dimMagenta = "#b782da";
-        # dimRed = "#ff4884";
-        # dimYellow = "#ffdb85";
+        transparent = "#000000";
+        bg0 = "#1d2329";
+        bg1 = "#232b30";
+        bg2 = "#2b353a";
+        bg3 = "#323e44";
+        bg4 = "#3a474b";
+        bg5 = "#435150";
+        bg_visual = "#3f2935";
+        bg_red = "#3d2d32";
+        bg_green = "#2f3c33";
+        bg_blue = "#293e4b";
+        bg_yellow = "#39382f";
+
+        fg = "#dbcdac";
+        red = "#ec7678";
+        orange = "#ec966a";
+        yellow = "#e3c277";
+        green = "#a9c678";
+        aqua = "#7cc68e";
+        blue = "#77c1b7";
+        purple = "#de97bb";
+        grey0 = "#6b7974";
+        grey1 = "#849489";
+        grey2 = "#9caba0";
       in
       {
-        "ui.background" = background;
-        "ui.background.separator" = dimWhite;
-        "ui.cursor" = white;
-        "ui.cursor.normal" = white;
-        "ui.cursor.insert" = white;
-        "ui.cursor.select" = white;
-        "ui.cursor.match" = green;
-        "ui.cursor.primary" = white;
-        "ui.cursor.primary.normal" = white;
-        "ui.cursor.primary.insert" = white;
-        "ui.cursor.primary.select" = white;
-        "ui.debug.breakpoint" = brightRed;
-        "ui.debug.active" = brightBlue;
-        "ui.gutter" = dimBlack;
-        "ui.gutter.selected" = white;
-        "ui.highlight.frameline" = yellow;
-        "ui.linenr" = white;
-        "ui.linenr.selected" = brightWhite;
-        "ui.statusline" = foreground;
-        "ui.statusline.inactive" = dimWhite;
-        "ui.statusline.normal" = green;
-        "ui.statusline.insert" = blue;
-        "ui.statusline.select" = magenta;
-        "ui.statusline.separator" = dimWhite;
-        "ui.popup" = background;
-        "ui.popup.info" = cyan;
-        "ui.window" = black;
-        "ui.help" = foreground;
-        "ui.text" = foreground;
-        "ui.text.focus" = white;
-        "ui.text.inactive" = dimWhite;
-        "ui.text.info" = brightCyan;
-        "ui.virtual.ruler" = dimWhite;
-        "ui.virtual.whitespace" = dimWhite;
-        "ui.virtual.indent-guide" = dimWhite;
-        "ui.virtual.inlay-hint" = dimWhite;
-        "ui.virtual.inlay-hint.parameter" = brightCyan;
-        "ui.virtual.inlay-hint.type" = brightMagenta;
-        "ui.virtual.wrap" = dimWhite;
-        "ui.menu" = brightBlack;
-        "ui.menu.selected" = blue;
-        "ui.menu.scroll" = { fg = brightBlue; bg = dimBlue; };
-        "ui.selection" = brightBlue;
-        "ui.selection.primary" = brightBlue;
-        "ui.highlight" = brightYellow;
-        "ui.cursorline.primary" = dimWhite;
-        "ui.cursorline.secondary" = dimWhite;
-        "ui.cursorcolumn.primary" = dimWhite;
-        "ui.cursorcolumn.secondary" = dimWhite;
-        "warning" = brightYellow;
-        "error" = brightRed;
-        "info" = brightCyan;
-        "hint" = brightGreen;
-        "diagnostic" = { modifiers = [ "underlined" ]; };
-        "diagnostic.hint" = brightGreen;
-        "diagnostic.info" = brightCyan;
-        "diagnostic.warning" = brightYellow;
-        "diagnostic.error" = brightRed;
+        "type" = yellow;
+        "constant" = purple;
+        "constant.numeric" = purple;
+        "constant.character.escape" = orange;
+        "string" = green;
+        "string.regexp" = blue;
+        "comment" = grey0;
+        "variable" = fg;
+        "variable.builtin" = blue;
+        "variable.parameter" = fg;
+        "variable.other.member" = fg;
+        "label" = aqua;
+        "punctuation" = grey2;
+        "punctuation.delimiter" = grey2;
+        "punctuation.bracket" = fg;
+        "keyword" = red;
+        "keyword.directive" = aqua;
+        "operator" = orange;
+        "function" = green;
+        "function.builtin" = blue;
+        "function.macro" = aqua;
+        "tag" = yellow;
+        "namespace" = aqua;
+        "attribute" = aqua;
+        "constructor" = yellow;
+        "module" = blue;
+        "special" = orange;
+
+        "markup.heading.marker" = grey2;
+        "markup.heading.1" = { fg = red; modifiers = [ "bold" ]; };
+        "markup.heading.2" = { fg = orange; modifiers = [ "bold" ]; };
+        "markup.heading.3" = { fg = yellow; modifiers = [ "bold" ]; };
+        "markup.heading.4" = { fg = green; modifiers = [ "bold" ]; };
+        "markup.heading.5" = { fg = blue; modifiers = [ "bold" ]; };
+        "markup.heading.6" = { fg = fg; modifiers = [ "bold" ]; };
+        "markup.list" = red;
+        "markup.bold" = { modifiers = [ "bold" ]; };
+        "markup.italic" = { modifiers = [ "italic" ]; };
+        "markup.link.url" = { fg = blue; modifiers = [ "underlined" ]; };
+        "markup.link.text" = purple;
+        "markup.quote" = grey2;
+        "markup.raw" = green;
+
+        "diff.plus" = green;
+        "diff.delta" = orange;
+        "diff.minus" = red;
+
+        "ui.background" = { bg = bg0; };
+        "ui.background.separator" = grey0;
+        "ui.cursor" = { fg = bg0; bg = fg; };
+        "ui.cursor.match" = { fg = orange; bg = bg_yellow; };
+        "ui.cursor.insert" = { fg = bg0; bg = grey1; };
+        "ui.cursor.select" = { fg = bg0; bg = blue; };
+        "ui.cursorline.primary" = { bg = bg1; };
+        "ui.cursorline.secondary" = { bg = bg1; };
+        "ui.selection" = { bg = bg3; };
+        "ui.linenr" = grey0;
+        "ui.linenr.selected" = fg;
+        "ui.statusline" = { fg = grey2; bg = bg3; };
+        "ui.statusline.inactive" = { fg = grey0; bg = bg1; };
+        "ui.statusline.normal" = { fg = bg0; bg = grey2; modifiers = [ "bold" ]; };
+        "ui.statusline.insert" = { fg = bg0; bg = yellow; modifiers = [ "bold" ]; };
+        "ui.statusline.select" = { fg = bg0; bg = blue; modifiers = [ "bold" ]; };
+        "ui.bufferline" = { fg = grey0; bg = bg1; };
+        "ui.bufferline.active" = { fg = fg; bg = bg3; modifiers = [ "bold" ]; };
+        "ui.popup" = { fg = grey2; bg = bg2; };
+        "ui.window" = { fg = grey0; bg = bg0; };
+        "ui.help" = { fg = fg; bg = bg2; };
+        "ui.text" = fg;
+        "ui.text.focus" = fg;
+        "ui.menu" = { fg = fg; bg = bg3; };
+        "ui.menu.selected" = { fg = bg0; bg = green; };
+        "ui.virtual.whitespace" = { fg = bg4; };
+        "ui.virtual.indent-guide" = { fg = bg4; };
+        "ui.virtual.ruler" = { bg = bg3; };
+
+        "hint" = blue;
+        "info" = aqua;
+        "warning" = yellow;
+        "error" = red;
+        "diagnostic" = { underline = { style = "curl"; }; };
+        "diagnostic.hint" = { underline = { color = blue; style = "dotted"; }; };
+        "diagnostic.info" = { underline = { color = aqua; style = "dotted"; }; };
+        "diagnostic.warning" = { underline = { color = yellow; style = "curl"; }; };
+        "diagnostic.error" = { underline = { color = red; style = "curl"; }; };
       };
   };
 
@@ -210,7 +234,19 @@ in
   programs.direnv.nix-direnv.enable = true;
   programs.nushell.enable = true;
   programs.nushell.extraEnv = ''
-    def-env ensure-path [new_path: string] {
+    def-env append-path [new_path string] {
+      let updated_env_path = (
+        if ($env.PATH | split row ":" | any { |it| $it == $new_path }) {
+          $env.PATH
+        }
+        else {
+          $"($env.PATH):($new_path)"
+        }
+      )
+      let-env PATH = $updated_env_path
+    }
+
+    def-env prepend-path [new_path string] {
       let updated_env_path = (
         if ($env.PATH | split row ":" | any { |it| $it == $new_path }) {
           $env.PATH
@@ -222,10 +258,10 @@ in
       let-env PATH = $updated_env_path
     }
 
-    ensure-path "/home/${username}/scripts"
-    ensure-path "/home/${username}/bin"
-    ensure-path "scripts"
-    ensure-path "bin"
+    prepend-path "/home/${username}/scripts"
+    prepend-path "/home/${username}/bin"
+    prepend-path "scripts"
+    prepend-path "bin"
   '';
   programs.nushell.extraConfig = ''
     let-env config = {
