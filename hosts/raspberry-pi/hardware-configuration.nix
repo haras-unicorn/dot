7b70405fe4f75fbe0ee7eeb5b48b5c2caeb34d15
Hrvoje Juric <social@hrvojej.anonaddy.me>
2023-08-20 19:20:03 +0000

    WIP

--- hosts/raspberry-pi/hardware-configuration.nix
+++ hosts/raspberry-pi/hardware-configuration.nix
@@ -1,9 +1,12 @@
-{ nixos-hardware, ... }:
+{ nixos-hardware, modulesPath, ... }:
 
 {
   imports = [
     nixos-hardware.nixosModules.raspberry-pi-4
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
+
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1008362877  
   nixpkgs.overlays = [
