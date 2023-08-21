a5fe86d268137661db891df3d5d9818c823bd7c4
Hrle <social@hrvojej.anonaddy.me>
2023-08-21 12:52:14 +0000

    WIP

--- hosts/raspberry-pi/configuration.nix
+++ hosts/raspberry-pi/configuration.nix
@@ -36,6 +36,13 @@
       };
     }
   ];
+  services.postgresql.authentication = pkgs.lib.mkOverride 10 ''
+    # TYPE    DATABASE    USER        ADDRESS         METHOD  OPTIONS
+    local     all         postgres                    peer
+    local     all         all                         peer
+    host      all         all         127.0.0.1/32    md5
+    host      all         all         ::1/128         md5
+  '';
   services.postgresql.enableTCPIP = true;
 
   users.users.pi.isNormalUser = true;
