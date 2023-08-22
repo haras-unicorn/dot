664739440cb25efd615949397dc6157383c066e4
Hrle <social@hrvojej.anonaddy.me>
2023-08-22 15:05:22 +0000

    WIP

--- hosts/raspberry-pi/configuration.nix
+++ hosts/raspberry-pi/configuration.nix
@@ -26,7 +26,7 @@ let
     };
 in
 {
-  import = [ <sops-nix/modules/sops> ];
+  imports = [ <sops-nix/modules/sops> ];
 
   sops.defaultSopsFile = ./secrets/secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
