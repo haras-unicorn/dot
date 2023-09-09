f4ca4cfd7ea4f188ec11e2bc6e7747303f447ce8
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 15:30:56 +0200

    add virtualisation stuff

--- src/host/desktop/configuration.nix
+++ src/host/desktop/configuration.nix
@@ -1,15 +1,17 @@
-{ pkgs, ... }:
+{ self, pkgs, ... }:
 
 {
   imports = [
-    ../../module/system/grub/grub.nix
-    ../../module/system/plymouth/plymouth.nix
-    ../../module/system/location/location.nix
-    ../../module/system/network/network.nix
-    ../../module/system/pipewire/pipewire.nix
-    ../../module/system/xserver/xserver.nix
-    ../../module/system/fonts/fonts.nix
-    ../../module/system/sudo/sudo.nix
+    "${self}/src/module/system/grub/grub.nix"
+    "${self}/src/module/system/plymouth/plymouth.nix"
+    "${self}/src/module/system/location/location.nix"
+    "${self}/src/module/system/network/network.nix"
+    "${self}/src/module/system/pipewire/pipewire.nix"
+    "${self}/src/module/system/xserver/xserver.nix"
+    "${self}/src/module/system/fonts/fonts.nix"
+    "${self}/src/module/system/sudo/sudo.nix"
+    "${self}/src/module/system/virtual/virtual.nix"
+    "${self}/src/module/system/windows/windows.nix"
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
