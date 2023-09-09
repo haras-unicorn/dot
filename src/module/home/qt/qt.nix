7f4862afc3fa298c4586a93cc7c2702e2e7f4719
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 15:27:15 +0200

    qt

--- src/module/home/qt/qt.nix
+++ src/module/home/qt/qt.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
 
 {
+  home.sessionVariables = {
+    # TODO: not working cuz nushell?
+    QT_QPA_PLATFORMTHEME = "gtk2";
+  };
+
   qt.enable = true;
-  qt.platformTheme = "gtk2";
+  qt.platformTheme = "gtk";
   qt.style.name = "Sweet-Dark";
   qt.style.package = pkgs.sweet;
 }
