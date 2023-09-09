8eb09e4907a39705e903e7509e0db76ebd4c3f91
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 20:16:33 +0200

    WIP

--- src/distro/de/de.nix
+++ src/distro/de/de.nix
@@ -1,14 +1,11 @@
-{ self, pkgs, ... }:
+{ self, ... }:
 
 {
   imports = [
     "${self}/src/module/home/hyprland/hyprland.nix"
     "${self}/src/module/home/dunst/dunst.nix"
     "${self}/src/module/home/wofi/wofi.nix"
-  ];
-
-  home.packages = with pkgs; [
-    waybar
-    swww
+    "${self}/src/module/home/swww/swww.nix"
+    "${self}/src/module/home/eww/eww.nix"
   ];
 }
--- src/module/system/wayland/wayland.nix
+++ src/module/system/wayland/wayland.nix
@@ -1,15 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
   };
-
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
-  ];
 
   programs.regreet.enable = true;
   programs.hyprland.enable = true;
