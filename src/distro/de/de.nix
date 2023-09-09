98d264fe2b366880be9bd98671e6f6f0108bcf21
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 18:47:58 +0200

    WIP

--- src/distro/de/de.nix
+++ src/distro/de/de.nix
@@ -1,7 +1,12 @@
-{ self, ... }:
+{ self, pkgs, ... }:
+
 {
   imports = [
     "${self}/src/module/home/hyprland/hyprland.nix"
     "${self}/src/module/home/dunst/dunst.nix"
+  ];
+
+  home.packages = with pkgs; [
+    waybar
   ];
 }
