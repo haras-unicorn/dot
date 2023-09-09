7d1b04ec48fbcdbfcda51f88066652100428ccd0
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 15:11:44 +0200

    fix fonts

--- src/module/system/fonts/fonts.nix
+++ src/module/system/fonts/fonts.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 {
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
+  fonts.enableDefaultPackages = true;
+  fonts.enableGhostscriptFonts = true;
   fonts.fontDir.enable = true;
-  fonts.enableDefaultFonts = true;
 }
