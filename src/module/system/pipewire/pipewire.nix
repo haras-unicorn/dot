c77c9a5d3ca41d40a2ef91ed4c4dc97f62c78579
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 18:52:20 +0200

    wIP

--- src/module/system/pipewire/pipewire.nix
+++ src/module/system/pipewire/pipewire.nix
@@ -6,10 +6,14 @@
     playerctl
   ];
 
+  sound.enable = true;
+
   security.rtkit.enable = true;
+
   services.pipewire.enable = true;
   services.pipewire.wireplumber.enable = true;
   services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
   services.pipewire.jack.enable = true;
   services.pipewire.pulse.enable = true;
 }
