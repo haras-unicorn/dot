25362ec54272726712a33d82e6208f69ea0799ac
Hrle <social@hrvojej.anonaddy.me>
2023-09-09 20:10:05 +0200

    WIP

--- src/module/home/hyprland/hyprland.nix
+++ src/module/home/hyprland/hyprland.nix
@@ -9,7 +9,21 @@
       $mainMod = SUPER
 
       bind = $mainMod, Q, exec, kitty
-      bind = $mainMod, Enter, exec, wofi --show drun
+      bind = $mainMod, E, exec, wofi --show drun
+
+      bind = $mainMod, right, movefocus, r
+      bind = $mainMod, left, movefocus, l
+      bind = $mainMod, up, movefocus, u
+      bind = $mainMod, down, movefocus, d
+
+      bind = $mainMod, R, submap, resize
+      submap = resize
+      binde = , right, resizeactive, 10 0
+      binde = , left, resizeactive, -10 0
+      binde = , up, resizeactive, 0 -10
+      binde = , down, resizeactive, 0 10
+      bind = , escape, submap, reset
+      submap = reset
     '';
   };
 }
