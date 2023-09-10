30af1984b979a85ad1c899e153ee475edfa481f6
Hrle <social@hrvojej.anonaddy.me>
2023-09-10 19:20:17 +0200

    WIP

--- src/module/home/qt/qt.nix
+++ src/module/home/qt/qt.nix
@@ -1,9 +1,13 @@
-{ sweet-theme, ... }:
+{ pkgs, sweet-theme, ... }:
 
 {
   qt.enable = true;
   qt.platformTheme = "kde";
   qt.style.name = "kvantum";
+  qt.style.package = with pkgs; [
+    libsForQt5.qtstyleplugin-kvantum
+    qt6Packages.qtstyleplugin-kvantum
+  ];
   xdg.configFile."Kvantum/Sweet".source = "${sweet-theme}/kde/Kvantum/Sweet";
   xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
     [General]
