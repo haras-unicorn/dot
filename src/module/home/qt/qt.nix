87431b1feedba666997413cc48bd03b4487ad177
Hrle <social@hrvojej.anonaddy.me>
2023-09-10 19:03:07 +0200

    WIP

--- src/module/home/qt/qt.nix
+++ src/module/home/qt/qt.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }:
+{ sweet-theme, ... }:
 
 {
   qt.enable = true;
-  qt.platformTheme = "gtk";
-  qt.style.name = "Sweet-Dark";
-  qt.style.package = pkgs.sweet;
+  qt.style.name = "kvantum";
+  xdg.configFile."Kvantum/Sweet".source = "${sweet-theme}/kde/Kvantum/Sweet";
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=Sweet
+  '';
+
   wayland.windowManager.hyprland.extraConfig = ''
     env = QT_QPA_PLATFORM,wayland
   '';
