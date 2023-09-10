b18d4401e54f0ee53dfaf8edde430a0df9c8d7f2
Hrle <social@hrvojej.anonaddy.me>
2023-09-10 12:47:54 +0200

    WIP

--- src/module/home/eww/eww.nix
+++ src/module/home/eww/eww.nix
@@ -4,4 +4,5 @@
   programs.eww.enable = true;
   programs.eww.package = pkgs.eww-wayland;
   programs.eww.configDir = ./config;
+  wayland.windowManager.hyprland.extraConfig = ''exec-once = ${pkgs.eww-wayland} daemon'';
 }
--- src/module/home/swww/swww.nix
+++ src/module/home/swww/swww.nix
@@ -4,4 +4,5 @@
   home.packages = with pkgs; [
     swww
   ];
+  wayland.windowManager.hyprland.extraConfig = ''exec-once = ${pkgs.swww} init'';
 }
