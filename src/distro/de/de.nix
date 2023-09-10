8f8f376be1bafd59421f124cb5c7ad9b87228396
Hrle <social@hrvojej.anonaddy.me>
2023-09-10 12:16:11 +0200

    WIP

--- src/distro/de/de.nix
+++ src/distro/de/de.nix
@@ -3,8 +3,7 @@
 {
   imports = [
     "${self}/src/module/home/hyprland/hyprland.nix"
-    "${self}/src/module/home/dunst/dunst.nix"
-    "${self}/src/module/home/wofi/wofi.nix"
+    "${self}/src/module/home/mako/mako.nix"
     "${self}/src/module/home/swww/swww.nix"
     "${self}/src/module/home/eww/eww.nix"
   ];
