3041bd5f587cc5620a21be4aeb778292f00852ed
Hrle <social@hrvojej.anonaddy.me>
2023-09-10 14:18:34 +0200

    WIP

--- src/module/system/wayland/wayland.nix
+++ src/module/system/wayland/wayland.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
   };
+
+  environment.systemPackages = with pkgs; [
+    wev
+  ];
 
   programs.regreet.enable = true;
   programs.hyprland.enable = true;
