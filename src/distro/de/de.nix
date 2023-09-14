4cea0c46cfe0ced897570fbd026afdb109a410e9
Hrle <social@hrvojej.anonaddy.me>
2023-09-14 20:42:12 +0200

    WIP

--- src/distro/de/de.nix
+++ src/distro/de/de.nix
@@ -3,11 +3,12 @@
 {
   imports = [
     "${self}/src/module/home/hyprland/hyprland.nix"
+    "${self}/src/module/home/waybar/waybar.nix"
+    "${self}/src/module/home/wlogout/wlogout.nix"
     "${self}/src/module/home/mako/mako.nix"
+    "${self}/src/module/home/wofi/wofi.nix"
     "${self}/src/module/home/swww/swww.nix"
     "${self}/src/module/home/eww/eww.nix"
-    "${self}/src/module/home/wofi/wofi.nix"
-    "${self}/src/module/home/waybar/waybar.nix"
     "${self}/src/module/home/obs-studio/obs-studio.nix"
     "${self}/src/module/home/grim/grim.nix"
     "${self}/src/module/home/brightnessctl/brightnessctl.nix"
