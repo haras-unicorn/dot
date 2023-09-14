c2393f023f48c123c02ab488662eca50a6eb7753
Hrle <social@hrvojej.anonaddy.me>
2023-09-15 00:41:09 +0200

    WIP

--- src/module/home/swww/swww.nix
+++ src/module/home/swww/swww.nix
@@ -1,24 +1,36 @@
 { self, pkgs, ... }:
 
 let
-  swww-reload =
+  colorap = pkgs.writeShellApplication {
+    name = "colorap";
+    runtimeInputs = [ pkgs.coreutils-full ];
+    text = ''
+      # TODO: "event listener" API so other modules can hook into it
+    '';
+  };
+
+  shwal =
     pkgs.writeShellApplication {
-      name = "swww-reload";
-      runtimeInputs = [ pkgs.coreutils-full pkgs.swww ];
+      name = "shwal";
+      runtimeInputs = [ pkgs.coreutils-full pkgs.swww pkgs.pywal colorap ];
       text = ''
-        swww img "$(find "${self}/assets/wallpapers" -type f | shuf -n 1)"
+        image = "$(find "${self}/assets/wallpapers" -type f | shuf -n 1)"
+        swww img "$image"
+        wal -steq -i "$image" -o colorap
       '';
     };
 in
 {
   home.packages = with pkgs; [
+    pywal
     swww
-    swww-reload
+    shwal
+    colorap
   ];
 
   wayland.windowManager.hyprland.extraConfig = ''
     exec-once = ${pkgs.swww}/bin/swww init
-    exec = ${swww-reload}/bin/swww-reload
+    exec = ${shwal}/bin/shwal
 
     misc {
       disable_hyprland_logo = true
