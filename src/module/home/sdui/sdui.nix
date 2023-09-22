b9c5c71e189cb85aa804ace972fa7dacff03a85f
Haras <social@hrvojej.anonaddy.me>
2023-09-23 01:17:48 +0200

    WIP

--- src/module/home/sdui/sdui.nix
+++ src/module/home/sdui/sdui.nix
@@ -31,7 +31,10 @@ let
 
     # TODO: use flake from webui-nix without copying and cd
     cp \
-      "${config.xdg.dataHome}/automatic1111-webui-nix"/flake.* \
+      "${config.xdg.dataHome}/automatic1111-webui-nix"/*.nix \
+      "${config.xdg.dataHome}/stable-diffusion-webui"
+    cp \
+      "${config.xdg.dataHome}/automatic1111-webui-nix"/flake.lock \
       "${config.xdg.dataHome}/stable-diffusion-webui"
     wd="$(pwd)"
     cd "${config.xdg.dataHome}/stable-diffusion-webui"
