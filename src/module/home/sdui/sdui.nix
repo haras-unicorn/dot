bc23d83f2502373e60a42eb92c7aefd67f4f9fb7
Haras <social@hrvojej.anonaddy.me>
2023-09-23 02:34:19 +0200

    WIP

--- src/module/home/sdui/sdui.nix
+++ src/module/home/sdui/sdui.nix
@@ -8,6 +8,7 @@ let
     args="$args --opt-sdp-no-mem-attention"
     args="$args --no-half-vae"
     args="$args --update-all-extensions"
+    args="$args --skip-torch-cuda-test"
     export COMMANDLINE_ARGS="$args"
     echo "$COMMANDLINE_ARGS"
 
