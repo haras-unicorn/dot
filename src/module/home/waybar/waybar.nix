3ed60ef77d6ba53877ecd6bf87d83b75b4f3b369
Haras <social@hrvojej.anonaddy.me>
2023-09-23 16:56:09 +0200

    WIP

--- src/module/home/waybar/waybar.nix
+++ src/module/home/waybar/waybar.nix
@@ -33,11 +33,22 @@ in
 
   xdg.configFile."walapp/waybar".source = "${waybare}/bin/waybare";
   xdg.configFile."walapp/waybar".executable = true;
+
   programs.lulezojne.config = {
     plop = [
       {
         template = ''
           @define-color background {{ rgba (set-alpha ansi.main.black 0.3) }};
+          @define-color foreground {{ hex ansi.main.black }};
+
+          @define-color black {{ rgba (set-alpha ansi.main.black 0.7) }};
+          @define-color white {{ hex ansi.main.white }};
+
+          @define-color red {{ hex ansi.main.red }}
+          @define-color green {{ hex ansi.main.green }}
+          @define-color blue {{ hex ansi.main.blue }}
+          @define-color cyan {{ hex ansi.main.cyan }}
+          @define-color yellow {{ hex ansi.main.yellow }}
           @define-color magenta {{ hex ansi.main.magenta }};
         '';
         "in" = "${config.xdg.configHome}/waybar/colors.css";
@@ -47,6 +58,5 @@ in
 
   wayland.windowManager.hyprland.extraConfig = ''
     exec-once = ${pkgs.waybar}/bin/waybar
-    exec = ${waybare}/bin/waybare
   '';
 }
