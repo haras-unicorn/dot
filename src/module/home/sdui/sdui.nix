407b78e2e59abe7961547e648a150b4ee18cc87d
Haras <social@hrvojej.anonaddy.me>
2023-09-23 03:25:44 +0200

    WIP

--- src/module/home/sdui/sdui.nix
+++ src/module/home/sdui/sdui.nix
@@ -45,6 +45,9 @@ let
     cp \
       "${config.xdg.dataHome}/automatic1111-webui-nix"/*.nix \
       "${config.xdg.dataHome}/stable-diffusion-webui"
+    cp \
+      "${config.xdg.dataHome}/automatic1111-webui-nix"/flake.lock \
+      "${config.xdg.dataHome}/stable-diffusion-webui"
     wd="$(pwd)"
     cd "${config.xdg.dataHome}/stable-diffusion-webui"
     nix flake lock
