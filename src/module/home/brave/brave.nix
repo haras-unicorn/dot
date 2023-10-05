f725e77f8c80e11b1c67c6698ec5f35c8d514219
Haras <social@hrvojej.anonaddy.me>
2023-10-05 19:55:07 +0200

    WIP

--- src/module/home/brave/brave.nix
+++ src/module/home/brave/brave.nix
@@ -8,14 +8,13 @@
       buildInputs = [ pkgs.makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/brave \
-          --append-flags \
-            --use-gl=egl \
-            --ozone-platform-hint=auto \
-            --enable-features=VaapiVideoDecoder \
-            --disable-features=UseChromeOSDirectVideoDecoder \
-            --enable-flag=ignore-gpu-blocklist \
-            --enable-flag=enable-gpu-rasterization \
-            --enable-flag=enable-zero-copy
+          --append-flags --use-gl=egl \
+          --append-flags --ozone-platform-hint=auto \
+          --append-flags --enable-features=VaapiVideoDecoder \
+          --append-flags --disable-features=UseChromeOSDirectVideoDecoder \
+          --append-flags --enable-flag=ignore-gpu-blocklist \
+          --append-flags --enable-flag=enable-gpu-rasterization \
+          --append-flags --enable-flag=enable-zero-copy
       '';
     })
   ];
