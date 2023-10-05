8d1287f9e427946e0aa4258d08debd0d3a009dcd
Haras <social@hrvojej.anonaddy.me>
2023-10-05 21:18:21 +0200

    WIP

--- src/module/system/wayland/wayland.nix
+++ src/module/system/wayland/wayland.nix
@@ -8,6 +8,7 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   environment.systemPackages = with pkgs; [
