51101db1e87693f992a94b62ad23a1a73297c19b
Haras <social@hrvojej.anonaddy.me>
2023-10-05 19:53:30 +0200

    WIP

--- src/module/home/brave/brave.nix
+++ src/module/home/brave/brave.nix
@@ -1,8 +1,23 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    brave
+  home.packages = [
+    (pkgs.symlinkJoin {
+      name = "brave";
+      paths = [ pkgs.brave ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/brave \
+          --append-flags \
+            --use-gl=egl \
+            --ozone-platform-hint=auto \
+            --enable-features=VaapiVideoDecoder \
+            --disable-features=UseChromeOSDirectVideoDecoder \
+            --enable-flag=ignore-gpu-blocklist \
+            --enable-flag=enable-gpu-rasterization \
+            --enable-flag=enable-zero-copy
+      '';
+    })
   ];
 
   wayland.windowManager.hyprland.extraConfig = ''
