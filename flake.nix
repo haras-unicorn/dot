8c7b3092cda8a798dcd0b9112facb16db1b98061
haras <social@hrvojej.anonaddy.me>
2023-10-13 00:50:54 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -85,9 +85,6 @@
                         nix.gc.automatic = true;
                         nix.gc.dates = "weekly";
                         nix.gc.options = "--delete-older-than 30d";
-
-                        nix.optimise.automatic = true;
-                        nix.optimise.dates = "weekly";
                         nix.settings.auto-optimise-store = true;
 
                         nix.settings.substituters = [
