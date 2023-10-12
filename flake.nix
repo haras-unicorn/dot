6cd0db085287ac9d48cf9bd625f9863a04d1c952
haras <social@hrvojej.anonaddy.me>
2023-10-13 00:48:01 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -81,8 +81,44 @@
                       ({ pkgs, ... }: {
                         nix.package = pkgs.nixFlakes;
                         nix.extraOptions = "experimental-features = nix-command flakes";
+
+                        nix.gc.automatic = true;
+                        nix.gc.dates = "weekly";
+                        nix.gc.options = "--delete-older-than 30d";
+
+                        nix.optimise.automatic = true;
+                        nix.optimise.dates = "weekly";
+                        nix.settings.auto-optimise-store = true;
+
+                        nix.settings.substituters = [
+                          "https://cache.nixos.org/"
+                          "https://haras.cachix.org/"
+                          "https://hyprland.cachix.org"
+                          "https://ai.cachix.org/"
+                        ];
+                        nis.settings.trusted-substituters = [
+                          "https://cache.nixos.org/"
+                          "https://haras.cachix.org/"
+                        ];
+                        nix.settings.trusted-public-keys = [
+                          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                          "haras.cachix.org-1:/HIo1JYqOIH1Nwk1EGXhuPPvDW0WekxIbY5CiXUZbYw="
+                          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                          "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+                        ];
+
+                        nix.settings.allowed-users = [
+                          "root"
+                          "@wheel"
+                        ];
+                        nix.settings.trusted-users = [
+                          "root"
+                        ];
+
                         nixpkgs.config = import "${self}/src/nixpkgs-config.nix";
+
                         networking.hostName = meta.hostname;
+
                         system.stateVersion = "23.11";
                       })
                       "${hosts}/${host}/hardware-configuration.nix"
---
+++ src/module/system/cachix/cachix.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  services.cachix-agent.enable = true;
+  services.cachix-agent.credentialsFile = "/etc/cachix-agent.token";
+  services.cachix-agent.host = "https://haras.cachix.org";
+}
