bb1a63f0c30abe9313c490aac80a1a71df857136
haras <social@hrvojej.anonaddy.me>
2023-10-14 22:04:22 +0200

    WIP

--- src/module/home/waybar/waybar.nix
+++ src/module/home/waybar/waybar.nix
@@ -36,12 +36,19 @@
           @define-color black {{ rgba ansi.main.black }};
           @define-color white {{ rgba ansi.main.white }};
 
-          @define-color red {{ hex ansi.main.bright_red }};
-          @define-color green {{ hex ansi.main.bright_green }};
-          @define-color blue {{ hex ansi.main.bright_blue }};
-          @define-color cyan {{ hex ansi.main.bright_cyan }};
-          @define-color yellow {{ hex ansi.main.bright_yellow }};
-          @define-color magenta {{ hex ansi.main.bright_magenta }};
+          @define-color red {{ hex ansi.main.red }};
+          @define-color green {{ hex ansi.main.green }};
+          @define-color blue {{ hex ansi.main.blue }};
+          @define-color cyan {{ hex ansi.main.cyan }};
+          @define-color yellow {{ hex ansi.main.yellow }};
+          @define-color magenta {{ hex ansi.main.magenta }};
+
+          @define-color bright-red {{ hex ansi.main.bright_red }};
+          @define-color bright-green {{ hex ansi.main.bright_green }};
+          @define-color bright-blue {{ hex ansi.main.bright_blue }};
+          @define-color bright-cyan {{ hex ansi.main.bright_cyan }};
+          @define-color bright-yellow {{ hex ansi.main.bright_yellow }};
+          @define-color bright-magenta {{ hex ansi.main.bright_magenta }};
         '';
         "in" = "${config.xdg.configHome}/waybar/colors.css";
         "then" = {
