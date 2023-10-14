5410feb1b80252f6c89d5fbce853d92c1ccdc504
haras <social@hrvojej.anonaddy.me>
2023-10-14 20:01:24 +0200

    WIP

--- src/module/system/network/network.nix
+++ src/module/system/network/network.nix
@@ -5,8 +5,8 @@
   networking.firewall.enable = true;
   networking.networkmanager.enable = true;
   networking.firewall.allowedTCPPorts = [
-    7860 # Syncthing
-    8384 # Stable diffusion
+    7860 # Stable diffusion
+    8384 # Syncthing
     5000 # ASP.NET Core HTTP
     5001 # ASP.NET Core HTTPS
   ];
