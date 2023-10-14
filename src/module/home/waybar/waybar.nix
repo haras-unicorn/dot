600555dbe5122b259cf5d5d715e0d61e81287b42
haras <social@hrvojej.anonaddy.me>
2023-10-14 20:50:15 +0200

    WIP

--- src/module/home/waybar/waybar.nix
+++ src/module/home/waybar/waybar.nix
@@ -25,12 +25,12 @@
           @define-color black {{ rgba (set-alpha ansi.main.black 0.5) }};
           @define-color white {{ rgba (set-alpha ansi.main.white 0.5) }};
 
-          @define-color red {{ hex ansi.main.red }};
-          @define-color green {{ hex ansi.main.green }};
-          @define-color blue {{ hex ansi.main.blue }};
-          @define-color cyan {{ hex ansi.main.cyan }};
-          @define-color yellow {{ hex ansi.main.yellow }};
-          @define-color magenta {{ hex ansi.main.magenta }};
+          @define-color red {{ hex ansi.main.brightRed }};
+          @define-color green {{ hex ansi.main.brightGreen }};
+          @define-color blue {{ hex ansi.main.brightBlue }};
+          @define-color cyan {{ hex ansi.main.brightCyan }};
+          @define-color yellow {{ hex ansi.main.brightYellow }};
+          @define-color magenta {{ hex ansi.main.brightMagenta }};
         '';
         "in" = "${config.xdg.configHome}/waybar/colors.css";
         "then" = {
