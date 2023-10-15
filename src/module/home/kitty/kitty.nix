0508c05b31dcfd401753b0d6d6b838a6b20c0844
haras <social@hrvojej.anonaddy.me>
2023-10-15 17:20:03 +0200

    WIP

--- src/module/home/kitty/kitty.nix
+++ src/module/home/kitty/kitty.nix
@@ -10,24 +10,28 @@
   programs.lulezojne.config.plop = [
     {
       template = ''
-        color0 {{ hex ansi.main.black }}
-        color8 {{ hex ansi.main.bright_black }}
-        color7  {{ hex ansi.main.white }}
-        color15 {{ hex ansi.main.bright_white }}
-        color3  {{ hex ansi.main.yellow }}
-        color11 {{ hex ansi.main.bright_yellow }}
-        color1 {{ hex ansi.main.red }}
-        color9 {{ hex ansi.main.bright_red }}
-        color5  {{ hex ansi.main.magenta }}
-        color13 {{ hex ansi.main.bright_magenta }}
-        color4  {{ hex ansi.main.blue }}
-        color12 {{ hex ansi.main.bright_blue }}
-        color6  {{ hex ansi.main.cyan }}
-        color14 {{ hex ansi.main.bright_cyan }}
+        color0  {{ hex ansi.main.black }}
+        color1  {{ hex ansi.main.red }}
         color2  {{ hex ansi.main.green }}
+        color3  {{ hex ansi.main.yellow }}
+        color4  {{ hex ansi.main.blue }}
+        color5  {{ hex ansi.main.magenta }}
+        color6  {{ hex ansi.main.cyan }}
+        color7  {{ hex ansi.main.white }}
+        color8  {{ hex ansi.main.bright_black }}
+        color9  {{ hex ansi.main.bright_red }}
         color10 {{ hex ansi.main.bright_green }}
+        color11 {{ hex ansi.main.bright_yellow }}
+        color12 {{ hex ansi.main.bright_blue }}
+        color13 {{ hex ansi.main.bright_magenta }}
+        color14 {{ hex ansi.main.bright_cyan }}
+        color15 {{ hex ansi.main.bright_white }}
       '';
       "in" = "${config.xdg.configHome}/kitty/colors.conf";
+      "then" = {
+        comand = "pkill";
+        args = [ "--signal" "SIGUSR1" "kitty" ];
+      };
     }
   ];
 
