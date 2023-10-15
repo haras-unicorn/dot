140e94d28b9a6a505660773b8d4d6b59d18dbee2
haras <social@hrvojej.anonaddy.me>
2023-10-15 19:54:59 +0200

    WIP

--- src/module/home/kitty/kitty.nix
+++ src/module/home/kitty/kitty.nix
@@ -10,23 +10,26 @@
   programs.lulezojne.config.plop = [
     {
       template = ''
-        color0  {{ hex ansi.main.black }}
-        color1  {{ hex (set-lightness ansi.main.red 0.7) }}
-        color2  {{ hex (set-lightness ansi.main.green 0.7) }}
-        color3  {{ hex (set-lightness ansi.main.yellow 0.7) }}
-        color4  {{ hex (set-lightness ansi.main.blue 0.7) }}
-        color5  {{ hex (set-lightness ansi.main.magenta 0.7) }}
-        color6  {{ hex (set-lightness ansi.main.cyan 0.7) }}
-        color7  {{ hex ansi.main.white }}
+        foreground {{ hex (set-lightness ansi.main.bright_white 0.9) }}
+        background {{ hex (set-lightness ansi.main.black 0.1) }}
 
-        color8  {{ hex ansi.main.bright_black }}
-        color9  {{ hex (set-lightness ansi.main.bright_red 0.9) }}
-        color10 {{ hex (set-lightness ansi.main.bright_green 0.9) }}
-        color11 {{ hex (set-lightness ansi.main.bright_yellow 0.9) }}
-        color12 {{ hex (set-lightness ansi.main.bright_blue 0.9) }}
-        color13 {{ hex (set-lightness ansi.main.bright_magenta 0.9) }}
-        color14 {{ hex (set-lightness ansi.main.bright_cyan 0.9) }}
-        color15 {{ hex ansi.main.bright_white }}
+        color0     {{ hex ansi.main.black }}
+        color1     {{ hex (set-lightness ansi.main.red 0.7) }}
+        color2     {{ hex (set-lightness ansi.main.green 0.7) }}
+        color3     {{ hex (set-lightness ansi.main.yellow 0.7) }}
+        color4     {{ hex (set-lightness ansi.main.blue 0.7) }}
+        color5     {{ hex (set-lightness ansi.main.magenta 0.7) }}
+        color6     {{ hex (set-lightness ansi.main.cyan 0.7) }}
+        color7     {{ hex ansi.main.white }}
+
+        color8     {{ hex ansi.main.bright_black }}
+        color9     {{ hex (set-lightness ansi.main.bright_red 0.9) }}
+        color10    {{ hex (set-lightness ansi.main.bright_green 0.9) }}
+        color11    {{ hex (set-lightness ansi.main.bright_yellow 0.9) }}
+        color12    {{ hex (set-lightness ansi.main.bright_blue 0.9) }}
+        color13    {{ hex (set-lightness ansi.main.bright_magenta 0.9) }}
+        color14    {{ hex (set-lightness ansi.main.bright_cyan 0.9) }}
+        color15    {{ hex ansi.main.bright_white }}
       '';
       "in" = "${config.xdg.configHome}/kitty/colors.conf";
       "then" = {
