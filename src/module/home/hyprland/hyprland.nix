7e4bec2dba8b13c17013af21c644c06c2e42c767
haras <social@hrvojej.anonaddy.me>
2023-10-15 20:19:56 +0200

    WIP

--- src/module/home/hyprland/hyprland.nix
+++ src/module/home/hyprland/hyprland.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.enableNvidiaPatches = true;
   wayland.windowManager.hyprland.xwayland.enable = true;
-  wayland.windowManager.hyprland.xwayland.extraConfig = ''
+  wayland.windowManager.hyprland.extraConfig = ''
     monitor = , preferred, auto, 1
     monitor = ${hardware.mainMonitor}, highrr, auto, 1
   
