ca524fcba0bc0f86807875dfb6dc64e18daaccdf
haras <social@hrvojej.anonaddy.me>
2023-10-15 19:20:05 +0200

    WIP

--- src/module/home/kitty/kitty.nix
+++ src/module/home/kitty/kitty.nix
@@ -11,20 +11,21 @@
     {
       template = ''
         color0  {{ hex ansi.main.black }}
-        color1  {{ hex ansi.main.red }}
-        color2  {{ hex ansi.main.green }}
-        color3  {{ hex ansi.main.yellow }}
-        color4  {{ hex ansi.main.blue }}
-        color5  {{ hex ansi.main.magenta }}
-        color6  {{ hex ansi.main.cyan }}
+        color1  {{ hex (set-lightness ansi.main.red 0.6) }}
+        color2  {{ hex (set-lightness ansi.main.green 0.6) }}
+        color3  {{ hex (set-lightness ansi.main.yellow 0.6) }}
+        color4  {{ hex (set-lightness ansi.main.blue 0.6) }}
+        color5  {{ hex (set-lightness ansi.main.magenta 0.6) }}
+        color6  {{ hex (set-lightness ansi.main.cyan 0.6) }}
         color7  {{ hex ansi.main.white }}
+
         color8  {{ hex ansi.main.bright_black }}
-        color9  {{ hex ansi.main.bright_red }}
-        color10 {{ hex ansi.main.bright_green }}
-        color11 {{ hex ansi.main.bright_yellow }}
-        color12 {{ hex ansi.main.bright_blue }}
-        color13 {{ hex ansi.main.bright_magenta }}
-        color14 {{ hex ansi.main.bright_cyan }}
+        color9  {{ hex (set-lightness ansi.main.bright_red 0.8) }}
+        color10 {{ hex (set-lightness ansi.main.bright_green 0.8) }}
+        color11 {{ hex (set-lightness ansi.main.bright_yellow 0.8) }}
+        color12 {{ hex (set-lightness ansi.main.bright_blue 0.8) }}
+        color13 {{ hex (set-lightness ansi.main.bright_magenta 0.8) }}
+        color14 {{ hex (set-lightness ansi.main.bright_cyan 0.8) }}
         color15 {{ hex ansi.main.bright_white }}
       '';
       "in" = "${config.xdg.configHome}/kitty/colors.conf";
