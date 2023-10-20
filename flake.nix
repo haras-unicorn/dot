38d30cd8642d93d4997eb9eaa2e80d1c5e3ca911
haras <social@hrvojej.anonaddy.me>
2023-10-20 16:41:19 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
 
                         networking.hostName = meta.hostname;
 
-                        system.stateVersion = "23.11";
+                        system.stateVersion = "23.05";
                       })
                       "${hosts}/${host}/hardware-configuration.nix"
                       "${hosts}/${host}/configuration.nix"
@@ -145,7 +145,7 @@
                               xdg.configFile."nixpkgs/config.nix".text = "${self}/src/nixpkgs-config.nix";
                               home.username = "${username}";
                               home.homeDirectory = "/home/${username}";
-                              home.stateVersion = "23.11";
+                              home.stateVersion = "23.05";
                               imports = [
                                 "${hosts}/${host}/home.nix"
                               ];
--- src/module/home/eza/eza.nix
+++ src/module/home/eza/eza.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.nushell.shellAliases = {
@@ -7,6 +7,11 @@
   };
 
   programs.eza.enable = true;
+
+  programs.nushell.shellAliases = {
+    eza = "${pkgs.eza}/bin/eza ${pkgs.lib.escapeShellArgs config.programs.eza.extraOptions}";
+  };
+
   programs.eza.extraOptions = [
     "--all"
     "--long"
