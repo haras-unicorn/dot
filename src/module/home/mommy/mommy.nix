53e653e4988acb466c86d12da5d6d446b2cde549
haras <social@hrvojej.anonaddy.me>
2023-10-21 12:41:51 +0200

    WIP

--- src/module/home/mommy/mommy.nix
+++ src/module/home/mommy/mommy.nix
@@ -6,7 +6,7 @@
   ];
 
   # TODO: better integration with starship
-  programs.nushell.environmentVariables = ''
+  programs.nushell.extraConfig = ''
     $env.PROMPT_COMMAND_RIGHT = { || ${pkgs.mommy}/bin/mommy -1 -s $env.LAST_EXIT_CODE }
   '';
 }
