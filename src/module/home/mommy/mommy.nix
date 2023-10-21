091c0990821b863078bcf1ef46135e33b307b7be
haras <social@hrvojej.anonaddy.me>
2023-10-21 13:09:32 +0200

    WIP

--- src/module/home/mommy/mommy.nix
+++ src/module/home/mommy/mommy.nix
@@ -1,12 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home.packages = with pkgs; [
     mommy
   ];
 
-  # TODO: better integration with starship
+  # TODO: better starship integration - this makes sure mommy is set after starship sets its things
   programs.nushell.extraConfig = ''
+    source "${config.xdg.configHome}/nushell/config.nu"
     $env.PROMPT_COMMAND_RIGHT = { || ${pkgs.mommy}/bin/mommy -1 -s $env.LAST_EXIT_CODE }
+    exit 0
   '';
 }
