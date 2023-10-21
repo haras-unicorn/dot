867600b445e69ef1b56fc964900abe73533d420e
haras <social@hrvojej.anonaddy.me>
2023-10-21 02:01:23 +0200

    WIP

--- src/host/desktop/configuration.nix
+++ src/host/desktop/configuration.nix
@@ -12,10 +12,11 @@
     "${self}/src/module/system/location/location.nix"
     "${self}/src/module/system/network/network.nix"
 
-    "${self}/src/module/system/pipewire/pipewire.nix"
-
     "${self}/src/module/system/ssh/ssh.nix"
     "${self}/src/module/system/keyring/keyring.nix"
+    "${self}/src/module/system/polkit/polkit.nix"
+
+    "${self}/src/module/system/pipewire/pipewire.nix"
 
     "${self}/src/module/system/fonts/fonts.nix"
     # "${self}/src/module/system/xserver/xserver.nix"
