d7f95ceb92955b0a3fa11b367cf4c710ea285497
haras <social@hrvojej.anonaddy.me>
2023-10-21 12:15:44 +0200

    WIP

--- src/module/home/mommy/mommy.nix
+++ src/module/home/mommy/mommy.nix
@@ -5,8 +5,7 @@
     mommy
   ];
 
-  # TODO: fix
-  programs.nushell.loginFile.text = ''
+  programs.nushell.extraConfig = ''
     $env.PROMPT_COMMAND_RIGHT = { || ${pkgs.mommy}/bin/mommy -1 -s $env.LAST_EXIT_CODE }
   '';
 }
