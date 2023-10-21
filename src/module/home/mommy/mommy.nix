31d103a608ff3dd83ffeceeb3896b09a269bef8f
haras <social@hrvojej.anonaddy.me>
2023-10-21 13:12:57 +0200

    WIP

--- src/module/home/mommy/mommy.nix
+++ src/module/home/mommy/mommy.nix
@@ -7,8 +7,11 @@
 
   # TODO: better starship integration - this makes sure mommy is set after starship sets its things
   programs.nushell.extraConfig = ''
-    source "${config.xdg.configHome}/nushell/config.nu"
-    $env.PROMPT_COMMAND_RIGHT = { || ${pkgs.mommy}/bin/mommy -1 -s $env.LAST_EXIT_CODE }
-    exit 0
+    if not $env.MOMMY_SOURCE {
+      $env.MOMMY_SOURCE = true
+      source "${config.xdg.configHome}/nushell/config.nu"
+      $env.PROMPT_COMMAND_RIGHT = { || ${pkgs.mommy}/bin/mommy -1 -s $env.LAST_EXIT_CODE }
+      exit 0
+    }
   '';
 }
