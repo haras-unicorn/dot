a74ed3d9423411368be61dac59ed6ab0af41ea5c
haras <social@hrvojej.anonaddy.me>
2023-10-21 21:04:34 +0200

    WIP

--- src/module/home/wlogout/wlogout.nix
+++ src/module/home/wlogout/wlogout.nix
@@ -1,5 +1,105 @@
-{ ... }:
+{ pkgs, config, ... }:
 
 {
+  home.packages = with pkgs; [
+    gtklock
+  ];
+
   programs.wlogout.enable = true;
+
+  programs.wlogout.layout = [
+    {
+      label = "lock";
+      action = "${pkgs.gtklock}/bin/gtklock";
+      text = "Lock";
+      keybind = "l";
+    }
+    {
+      label = "logout";
+      action = "loginctl terminate-user $USER";
+      text = "Logout";
+      keybind = "e";
+    }
+    {
+      label = "suspend";
+      action = "systemctl suspend";
+      text = "Suspend";
+      keybind = "u";
+    }
+    {
+      label = "hibernate";
+      action = "systemctl hibernate";
+      text = "Hibernate";
+      keybind = "h";
+    }
+    {
+      label = "reboot";
+      action = "systemctl reboot";
+      text = "Reboot";
+      keybind = "r";
+    }
+    {
+      label = "shutdown";
+      action = "systemctl poweroff";
+      text = "Shutdown";
+      keybind = "s";
+    }
+  ];
+
+  programs.wlogout.style = ''
+    @import "${config.xdg.configHome}/wlogout/colors.css";
+
+    #lock {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/lock.png"));
+    }
+
+    #logout {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/logout.png"));
+    }
+
+    #suspend {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/suspend.png"));
+    }
+
+    #hibernate {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/hibernate.png"));
+    }
+
+    #shutdown {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/shutdown.png"));
+    }
+
+    #reboot {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/reboot.png"));
+    }
+
+    ${builtins.readFile ./style.css}
+  '';
+
+  programs.lulezojne.config.plop = [
+    {
+      template = ''
+        @define-color background {{ rgba (set-alpha ansi.main.black 0.7) }};
+        @define-color foreground {{ hex ansi.main.bright_white }};
+
+        @define-color black {{ rgba ansi.main.black }};
+        @define-color white {{ rgba ansi.main.white }};
+
+        @define-color red {{ hex ansi.main.red }};
+        @define-color green {{ hex ansi.main.green }};
+        @define-color blue {{ hex ansi.main.blue }};
+        @define-color cyan {{ hex ansi.main.cyan }};
+        @define-color yellow {{ hex ansi.main.yellow }};
+        @define-color magenta {{ hex ansi.main.magenta }};
+
+        @define-color bright-red {{ hex ansi.main.bright_red }};
+        @define-color bright-green {{ hex ansi.main.bright_green }};
+        @define-color bright-blue {{ hex ansi.main.bright_blue }};
+        @define-color bright-cyan {{ hex ansi.main.bright_cyan }};
+        @define-color bright-yellow {{ hex ansi.main.bright_yellow }};
+        @define-color bright-magenta {{ hex ansi.main.bright_magenta }};
+      '';
+      "in" = "${config.xdg.configHome}/wlogout/colors.css";
+    }
+  ];
 }
