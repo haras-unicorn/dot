092ffdf700de4b111049d9666a192856abf023b4
haras <social@hrvojej.anonaddy.me>
2023-10-29 22:32:07 +0100

    WIP

--- src/module/home/firefox/firefox.nix
+++ src/module/home/firefox/firefox.nix
@@ -24,31 +24,32 @@
         darkreader
         vimium
       ];
-      search = {
-        default = "Google";
-        engines = {
-          "Bing".metaData.hidden = true;
-          "Google".metaData.alias = "@g";
-          "Nix Packages" = {
-            urls = [{
-              template = "https://search.nixos.org/packages";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
 
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = [ "@np" ];
-          };
-          "NixOS Wiki" = {
-            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = [ "@nw" ];
-          };
-        };
-      };
+      # TODO: fix firefox making this unbreedable
+      # search = {
+      #   default = "Google";
+      #   engines = {
+      #     "Bing".metaData.hidden = true;
+      #     "Google".metaData.alias = "@g";
+      #     "Nix Packages" = {
+      #       urls = [{
+      #         template = "https://search.nixos.org/packages";
+      #         params = [
+      #           { name = "type"; value = "packages"; }
+      #           { name = "query"; value = "{searchTerms}"; }
+      #         ];
+      #       }];
+      #       icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      #       definedAliases = [ "@np" ];
+      #     };
+      #     "NixOS Wiki" = {
+      #       urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+      #       iconUpdateURL = "https://nixos.wiki/favicon.png";
+      #       updateInterval = 24 * 60 * 60 * 1000; # every day
+      #       definedAliases = [ "@nw" ];
+      #     };
+      #   };
+      # };
     };
   };
 
