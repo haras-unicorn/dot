af3a1c4f702cff0222c8d61ef0749c78a66e4ef8
haras <social@hrvojej.anonaddy.me>
2023-10-29 15:43:00 +0100

    WIP

--- src/distro/app/app.nix
+++ src/distro/app/app.nix
@@ -2,19 +2,26 @@
 
 {
   imports = [
-    # TODO: somehow on qt and hyprland??
+    # TODO: wayland ...
     # "${self}/src/module/home/cursor/cursor.nix"
+
     "${self}/src/module/home/gtk/gtk.nix"
     "${self}/src/module/home/qt/qt.nix"
+
+    # TODO: wayland/gpu flags in appropriate places
     # "${self}/src/module/home/brave/brave.nix"
     # "${self}/src/module/home/chromium/chromium.nix"
     "${self}/src/module/home/vivaldi/vivaldi.nix"
-    "${self}/src/module/home/spotify/spotify.nix"
-    "${self}/src/module/home/kitty/kitty.nix"
+
     "${self}/src/module/home/syncthing/syncthing.nix"
     "${self}/src/module/home/keepassxc/keepassxc.nix"
     "${self}/src/module/home/ferdium/ferdium.nix"
+
+    "${self}/src/module/home/spotify/spotify.nix"
+
+    "${self}/src/module/home/kitty/kitty.nix"
     "${self}/src/module/home/code/code.nix"
+
     "${self}/src/module/home/daw/daw.nix"
   ];
 
