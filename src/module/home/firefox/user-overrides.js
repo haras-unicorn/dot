da7b548fc15521fdf6e4cbde78abb741db990c12
haras <social@hrvojej.anonaddy.me>
2023-10-31 01:15:08 +0100

    WIP

--- src/module/home/firefox/user-overrides.js
+++ src/module/home/firefox/user-overrides.js
@@ -1,6 +1,17 @@
+// no weird margins around websites
 user_pref("privacy.resistFingerprinting.letterboxing", false);
+
+// no bookmarks bar
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// dumbass icon
 user_pref("extensions.pocket.enabled", false);
+
+// disable the would you like to save the login credentials prompt 
 user_pref("signon.rememberSignons", false);
+
+// keep history
 user_pref("privacy.clearOnShutdown.history", false);
+
+// suggest search
 user_pref("browser.search.suggest.enabled", true);
