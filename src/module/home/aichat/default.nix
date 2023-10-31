c111aa2234673c681444a2cc7190827b9d504f98
haras <social@hrvojej.anonaddy.me>
2023-10-31 19:13:57 +0100

    WIP

--- src/module/home/aichat/default.nix
+++ src/module/home/aichat/default.nix
@@ -7,7 +7,15 @@ let
     text = ''
       cat <<EOF >${config.xdg.configHome}/aichat/config.yaml
         api_key: $(cat ${config.home.homeDirectory}/.openai/api.key)
-        save: true
+        model: gpt-3.5-turbo # LLM model
+        temperature: 1.0     # GPT temperature, between 0 and 2
+        save: true           # Whether to save the message
+        highlight: true      # Set false to turn highlight
+        light_theme: true    # Whether to use a light theme
+        wrap: 80             # Specify the text-wrapping mode (no, auto, <max-width>)
+        wrap_code: true      # Whether wrap code block
+        auto_copy: false     # Automatically copy the last output to the clipboard
+        keybindings: vi      # REPL keybindings. values: emacs, vi
       EOF
 
       aichat "$@"
