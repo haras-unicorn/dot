9fab791caf8920604bcff4b39042747a3bc27103
haras <social@hrvojej.anonaddy.me>
2023-10-31 19:00:14 +0100

    WIP

--- src/module/home/aichat/default.nix
+++ src/module/home/aichat/default.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  aichat = pkgs.writeShellApplication {
+    name = "aichat";
+    runtimeInputs = [ pkgs.aichat ];
+    text = ''
+      cat <<EOF >${config.xdg.configHome}/aichat/config.yaml
+        api_key: $(cat ${config.home.homeDirectory}/.openai/api.key)
+        save: true
+      EOF
+
+      aichat "$@"
+    '';
+
+  };
+in
 {
-  home.packages = with pkgs; [
-    aichat
-  ];
+  home.packages = [ aichat ];
 }
--- src/module/home/open-interpreter/default.nix
+++ src/module/home/open-interpreter/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 let
   open-interpreter = pkgs.writeShellApplication {
     name = "interpreter";
     runtimeInputs = [ pkgs.open-interpreter ];
     text = ''
-      interpreter -ak "$(cat ~/.openai/api.key)"
+      interpreter -ak "$(cat ${config.home.homeDirectory}/.openai/api.key)" "$@"
     '';
   };
 in
