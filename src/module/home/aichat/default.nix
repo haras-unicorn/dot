2c3ef065f209dedb8bb299b66b5a04d2c8f480b2
haras <social@hrvojej.anonaddy.me>
2023-10-31 19:50:01 +0100

    WIP

--- src/module/home/aichat/default.nix
+++ src/module/home/aichat/default.nix
@@ -7,15 +7,7 @@ let
     text = ''
       cat <<EOF >${config.xdg.configHome}/aichat/config.yaml
         api_key: $(cat ${config.home.homeDirectory}/.openai/api.key)
-        model: gpt-3.5-turbo # LLM model
-        temperature: 1.0     # GPT temperature, between 0 and 2
-        save: true           # Whether to save the message
-        highlight: true      # Set false to turn highlight
-        light_theme: false   # Whether to use a light theme
-        wrap: 80             # Specify the text-wrapping mode (no, auto, <max-width>)
-        wrap_code: true      # Whether wrap code block
-        auto_copy: false     # Automatically copy the last output to the clipboard
-        keybindings: vi      # REPL keybindings. values: emacs, vi
+        ${builtins.readFile ./config.yaml}
       EOF
       chmod 600 ${config.xdg.configHome}/aichat/config.yaml
 
@@ -26,4 +18,6 @@ let
 in
 {
   home.packages = [ aichat ];
+
+  xdg.configFile."aichat/roles.yaml".source = ./roles.yaml;
 }
