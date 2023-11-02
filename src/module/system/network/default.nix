9910f89ded326633ecd2a15f8c372660c5e2eb82
haras <social@hrvojej.anonaddy.me>
2023-11-02 18:35:36 +0100

    WIP

--- src/module/system/network/default.nix
+++ src/module/system/network/default.nix
@@ -15,6 +15,9 @@
   environment.etc."ppp/options".text = ''
     ipcp-accept-remote
   '';
+  environment.etc."openfortivpn/config".text = ''
+    trusted-cert = 679706703490caf5ce655c3fa98e2e95309823b93526d62a47441fbe34502ac9
+  '';
   environment.systemPackages = with pkgs; [
     ppp
     openconnect_openssl
