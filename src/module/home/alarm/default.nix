0e0c06996b95ff3d7fdabf9daecd1d3c2fbeaaa0
haras <social@hrvojej.anonaddy.me>
2023-11-04 18:05:14 +0100

    WIP

--- src/module/home/alarm/default.nix
+++ src/module/home/alarm/default.nix
@@ -2,4 +2,9 @@
 
 {
   home.packages = with pkgs; [ alarm-clock-applet ];
+
+  # TODO: systemd
+  wayland.windowManager.hyprland.extraConfig = ''
+    exec-once = ${pkgs.alarm-clock-applet}/bin/alarm-clock-applet
+  '';
 } 
