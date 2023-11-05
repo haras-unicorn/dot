2b04120a54a4fe9c01a9957ecb672e8191227e22
haras <social@hrvojej.anonaddy.me>
2023-11-05 18:44:59 +0100

    WIP

--- src/module/home/gtk/default.nix
+++ src/module/home/gtk/default.nix
@@ -5,7 +5,7 @@
 # TODO: cursor theme?
 
 let
-  font = ''"${config.dot.font.sans.name} 15"'';
+  font = ''"${config.dot.font.sans.name}" 15'';
   ini2 = ''
     gtk-font-name = ${font}
     gtk-icon-theme-name = "BeautyLine"
