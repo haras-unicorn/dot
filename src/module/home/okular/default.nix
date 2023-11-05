8bce97bc5bec07f8e0373d9d65fbfb39294d3c89
haras <social@hrvojej.anonaddy.me>
2023-11-05 20:45:48 +0100

    WIP

---
+++ src/module/home/okular/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+let
+  desktop = "${pkgs.libsForQt5.okular}/share/applications/okular";
+  mime = {
+    "application/pdf" = desktop;
+  };
+in
+{
+  home.packages = with pkgs; [
+    libsForQt5.okular
+  ];
+
+  xdg.mimeApps.associations.added = mime;
+  xdg.mimeApps.defaultApplications = mime;
+}
