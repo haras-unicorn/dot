822769b940082ecc6f525fcc46c8a9278ba3e9fd
haras <social@hrvojej.anonaddy.me>
2023-11-14 13:23:13 +0100

    WIP

--- src/module/system/gaming/default.nix
+++ src/module/system/gaming/default.nix
@@ -10,6 +10,8 @@
     lutris
     retroarch
     cartridges
+    winetricks
+    protontricks
   ];
 
   programs.steam.enable = true;
--- src/module/system/virtual/default.nix
+++ src/module/system/virtual/default.nix
@@ -9,6 +9,7 @@
 {
   environment.systemPackages = with pkgs; [
     wineWowPackages.stable
+    winetricks
     virt-manager
     spice
     spice-vdagent
