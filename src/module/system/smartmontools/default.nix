e16d9c6e4d3c5ca4c3812ea382ab455f73627e3b
haras <social@hrvojej.anonaddy.me>
2023-11-23 19:40:46 +0100

    WIP

--- src/module/system/smartmontools/default.nix
+++ src/module/system/smartmontools/default.nix
@@ -2,6 +2,8 @@
 
 # TODO: collectd + postgre + timescaledb + prometheus + graphana stack
 # TODO: netdata?
+# TODO: check out tuptime?
+# TODO: https://github.com/tremor-rs/tremor-runtime - rust based stream processor
 
 {
   services.smartd.enable = true;
