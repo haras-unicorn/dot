6d772e4091a7dc3891df3f920fd7f3214f5024e1
haras <social@hrvojej.anonaddy.me>
2023-11-23 15:11:00 +0100

    WIP

--- src/distro/app/default.nix
+++ src/distro/app/default.nix
@@ -74,6 +74,7 @@ in
     "${self}/src/module/home/okular"
     "${self}/src/module/home/xarchiver"
     "${self}/src/module/home/discord"
+    "${self}/src/module/home/chromium"
 
     # "${self}/src/module/home/${config.dot.term.module}"
     # "${self}/src/module/home/${config.dot.visual.module}"
