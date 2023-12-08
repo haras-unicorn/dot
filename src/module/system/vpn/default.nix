d93d5544b71b3c0c554be1b5e2376a3ace7fd0a5
haras <social@haras.anonaddy.me>
2023-12-08 17:33:41 +0100

    WIP

--- src/module/system/vpn/default.nix
+++ src/module/system/vpn/default.nix
@@ -4,9 +4,9 @@
 
 {
   # NOTE: https://github.com/NixOS/nixpkgs/issues/231038
-  environment.etc."ppp/options".text = ''
-    ipcp-accept-remote
-  '';
+  # environment.etc."ppp/options".text = ''
+  #   ipcp-accept-remote
+  # '';
 
   environment.systemPackages = with pkgs; [
     ppp
@@ -16,6 +16,6 @@
     networkmanager-fortisslvpn
   ];
 
-  services.logmein-hamachi.enable = true;
-  programs.haguichi.enable = true;
+  # services.logmein-hamachi.enable = true;
+  # programs.haguichi.enable = true;
 }
