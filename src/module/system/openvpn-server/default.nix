347ee935daecbf053c3e5af1f628b2c653372d6e
haras <social@hrvojej.anonaddy.me>
2023-12-17 18:05:12 +0100

    WIP

--- src/module/system/openvpn-server/default.nix
+++ src/module/system/openvpn-server/default.nix
@@ -88,13 +88,13 @@ in
     sops.secrets."server.dhparam.pem".group = "nogroup";
     sops.secrets."server.dhparam.pem".mode = "0600";
   }
-    # // (builtins.foldl'
-    #   (clients: client: clients // {
-    #     environment.etc."/etc/openvpn/${cfg.host}/clients/${client}" = ''
-    #       ifconfig-push ${subnet}.${cfg.clients."${client}"} ${mask}
-    #     '';
-    #   })
-    #   ({ })
-    #   (builtins.attrNames cfg.clients))
+  // (lists.foldl'
+    (clients: client: clients // {
+      environment.etc."/etc/openvpn/${cfg.host}/clients/${client}" = ''
+        ifconfig-push ${subnet}.${cfg.clients."${client}"} ${mask}
+      '';
+    })
+    ({ })
+    (attrsets.attrNames cfg.clients))
   );
 }
