7fd4329a90f325f47150cfbef0b081ba08c45960
haras <social@haras.anonaddy.me>
2024-01-03 17:24:13 +0100

    WIP

--- src/module/home/spacedrive/default.nix
+++ src/module/home/spacedrive/default.nix
@@ -1,9 +1,4 @@
-{ pkgs, config, ... }:
-
-# TODO: use when more usable and stable
-
-# NOTE: https://github.com/NixOS/nixpkgs/issues/170254
-# configure and all when better
+{ pkgs, ... }:
 
 # NOTE: to remove:
 # - documents/.spacedrive
@@ -11,26 +6,13 @@
 # - .cache/spacedrive
 
 let
-  desktopEntry = pkgs.writeText "spacedrive.desktop" ''
-    [Desktop Entry]
-    Categories=Application;Filesystem
-    Exec=${config.nur.repos.mikaelfangel-nur.spacedrive}/bin/spacedrive %U
-    GenericName=Files
-    Name=Spacedrive
-    Terminal=false
-    Type=Application
-    Version=1.4  
-    MimeType=inode/directory
-  '';
   mime = {
-    # NOTE: like this because xdg has a problem with /nix/store hashes in filenames?
-    "inode/directory" = "${config.xdg.dataHome}/applications/spacedrive.desktop";
+    "inode/directory" = "${pkgs.spacedrive}/share/applications/spacedrive.desktop";
   };
 in
 {
-  home.packages = [ config.nur.repos.mikaelfangel-nur.spacedrive ];
+  home.packages = with pkgs; [ spacedrive ];
 
-  xdg.dataFile."applications/spacedrive.desktop".source = desktopEntry;
   xdg.mimeApps.associations.added = mime;
   xdg.mimeApps.defaultApplications = mime;
 }
