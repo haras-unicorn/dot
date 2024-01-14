3c871bb200bb70eacd7b90202159ec7fcd8bfe8d
haras <social@hrvojej.anonaddy.me>
2024-01-14 13:57:35 +0100

    WIP

--- src/module/home/teams/default.nix
+++ src/module/home/teams/default.nix
@@ -7,7 +7,7 @@ let
     paths = [ pkgs.teams-for-linux ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      wrapProgram $out/bin/teams \
+      wrapProgram $out/bin/teams-for-linux \
         --append-flags --enable-features=WebRTCPipeWireCapturer \
         --append-flags --enable-features=UseOzonePlatform \
         --append-flags --ozone-platform-hint=auto
