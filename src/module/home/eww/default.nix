084eed4195427890520edbda9e6903612246ef6d
haras <social@haras.anonaddy.me>
2024-01-21 11:11:48 +0100

    WIP

--- src/module/home/eww/default.nix
+++ src/module/home/eww/default.nix
@@ -2,12 +2,23 @@
 
 # TODO: use instead of waybar after https://github.com/elkowar/eww/pull/743
 
+let
+  pkg = pkgs.eww-wayland;
+in
 {
   de.sessionStartup = [
-    "${pkgs.eww-wayland}/bin/eww daemon"
+    "${pkg}/bin/eww daemon"
+  ];
+
+  de.keybinds = [
+    {
+      mods = [ "super" ];
+      key = "s";
+      command = "${pkg}/bin/eww open top-bar";
+    }
   ];
 
   programs.eww.enable = true;
-  programs.eww.package = pkgs.eww-wayland;
+  programs.eww.package = pkg;
   programs.eww.configDir = ./config;
 }
