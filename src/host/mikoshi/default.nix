42136290fdd7974cac654aeed69c2dc228b91469
haras <social@hrvojej.anonaddy.me>
2024-02-17 17:44:56 +0100

    WIP

--- src/host/mikoshi/default.nix
+++ src/host/mikoshi/default.nix
@@ -17,8 +17,12 @@
 
     boot.initrd.availableKernelModules = [
       "nvme"
+      "xhci_pci"
       "ahci"
+      "usb_storage"
+      "usbhid"
       "sd_mod"
+      "sr_mod"
     ];
 
     boot.initrd.kernelModules = [
