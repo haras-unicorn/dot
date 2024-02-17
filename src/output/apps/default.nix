d6ab53a3071d7660c67f1f5e9ad9e212789490f9
haras <social@hrvojej.anonaddy.me>
2024-02-17 12:24:25 +0100

    WIP

--- src/output/apps/default.nix
+++ src/output/apps/default.nix
@@ -3,9 +3,15 @@
 , ...
 }:
 
-flake-utils.lib.eachDefaultSystem (system: {
-  apps."${system}".default = {
-    type = "app";
-    program = "${self}/scripts/install";
-  };
-})
+let
+  configs = flake-utils.lib.defaultSystems;
+in
+builtins.foldl'
+  (apps: system: apps // {
+    apps."${system}".default = {
+      type = "app";
+      program = "${self}/scripts/install";
+    };
+  })
+  ({ })
+  configs
