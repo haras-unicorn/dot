f0a482ae42d0017cf54a344f45e5a41e5e65c700
haras <social@hrvojej.anonaddy.me>
2024-02-17 19:02:07 +0100

    WIP

--- src/host/hearth/default.nix
+++ src/host/hearth/default.nix
@@ -86,7 +86,7 @@
       "${self}/src/module/system/location"
       "${self}/src/module/system/network"
       "${self}/src/module/system/vpn"
-      # "${self}/src/module/system/openvpn-client"
+      "${self}/src/module/system/openvpn-client"
       "${self}/src/module/system/smartmontools"
 
       "${self}/src/module/system/sudo"
@@ -111,9 +111,9 @@
       8384 # syncthing
     ];
 
-    # dot.openvpn.client.enable = true;
-    # dot.openvpn.client.host = vpnHost;
-    # dot.openvpn.client.domain = vpnDomain;
+    dot.openvpn.client.enable = true;
+    dot.openvpn.client.host = vpnHost;
+    dot.openvpn.client.domain = vpnDomain;
   };
 
   user = { self, ... }: {
