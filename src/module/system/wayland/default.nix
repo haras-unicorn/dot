26829571f37d77f076cde1b23e231dbc953009bf
haras <social@hrvojej.anonaddy.me>
2024-02-22 11:52:56 +0100

    WIP

--- src/module/system/wayland/default.nix
+++ src/module/system/wayland/default.nix
@@ -8,9 +8,9 @@
 # FIXME: uncouple hyprland/greetd/gtklock/portals?
 
 # FIXME: links not opening https://github.com/flatpak/xdg-desktop-portal-gtk/issues/440
+# TODO: these commands on hyprland startup and make config for tuigreet/hyprland command
 # systemctl --user import-environment PATH
 # systemctl --user restart xdg-desktop-portal.service
-# dbus-run-session ...
 
 # FIXME: https://github.com/NVIDIA/open-gpu-kernel-modules/issues/467#issuecomment-1544340511
 
