b72e0fe45f9713663f63f12360d3906c1b1922cb
haras <social@hrvojej.anonaddy.me>
2024-02-25 17:25:38 +0100

    WIP

--- src/module/home/piper/default.nix
+++ src/module/home/piper/default.nix
@@ -8,25 +8,25 @@ let
       MODEL="$1"
       if [[ ! -f "$MODEL" ]]; then
         printf "I need a model to speak.\n"
-        return
+        exit 1
       fi
 
       CONFIG="$1.json"
       if [[ ! -f "$CONFIG" ]]; then
         printf "The provided model does not have a config.\n"
-        return
+        exit 1
       fi
 
       SAMPLERATE="$(jq .audio.sample_rate < "$CONFIG")"
       if [[ ! $SAMPLERATE =~ ^[0-9]+$ ]]; then
         printf "The provided model config does not include a sample rate.\n"
-        return
+        exit 1
       fi
 
       TEXT="$2"
       if [[ "$TEXT" == "" ]]; then
         printf "I need text to speak.\n"
-        return
+        exit 1
       fi
 
       echo "$TEXT" | \
