1b28edde56a2ab7ce6f85aad1503860fef68e4bb
haras <social@hrvojej.anonaddy.me>
2024-03-06 18:08:48 +0100

    WIP

--- src/module/system/hardened/default.nix
+++ src/module/system/hardened/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+# FIXME: https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501/7
+
 {
   boot.kernelPackages = pkgs.linuxPackages_hardened;
+  services.logrotate.checkConfig = false;
 }
