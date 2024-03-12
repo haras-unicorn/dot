9c961f73254c6516623183f658af3b1d9751d7c9
haras <social@hrvojej.anonaddy.me>
2024-03-12 21:39:47 +0100

    WIP

--- src/distro/app/default.nix
+++ src/distro/app/default.nix
@@ -34,7 +34,7 @@ let
   mime = browserMime // visualMime;
 
   jan-unwrapped = pkgs.appimageTools.wrapType2 {
-    name = "jan-unwrappedj";
+    name = "jan-unwrapped";
     src = pkgs.fetchurl {
       url = "https://github.com/janhq/jan/releases/download/v0.4.7/jan-linux-x86_64-0.4.7.AppImage";
       sha256 = "sha256-Mn7rIBEf46JbNof8h3z66TGdGKnb0FGMJc46JncA0KM=";
