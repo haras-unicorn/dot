112c2a8ddbe44cc7d1f16b40bb20413a63fd85b4
haras <social@hrvojej.anonaddy.me>
2024-03-12 20:06:29 +0100

    WIP

--- src/distro/app/default.nix
+++ src/distro/app/default.nix
@@ -33,13 +33,23 @@ let
 
   mime = browserMime // visualMime;
 
-  jan = pkgs.appimageTools.wrapType2 {
-    name = "jan";
+  jan-unwrapped = pkgs.appimageTools.wrapType2 {
+    name = "jan-unwrappedj";
     src = pkgs.fetchurl {
       url = "https://github.com/janhq/jan/releases/download/v0.4.7/jan-linux-x86_64-0.4.7.AppImage";
       sha256 = "sha256-Mn7rIBEf46JbNof8h3z66TGdGKnb0FGMJc46JncA0KM=";
     };
     extraPkgs = pkgs: [ ];
+  };
+
+  jan = pkgs.symlinkJoin {
+    name = "jan";
+    paths = [ jan-unwrapped ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/jan-unwrapped \
+        --set LD_LIBRARY_PATH "${pkgs.cudaPackages.cudatoolkit}/lib"
+    '';
   };
 in
 {
