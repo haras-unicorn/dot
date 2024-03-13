465554c3cd4e22bda0708c4f78c25e4db6231290
haras <social@hrvojej.anonaddy.me>
2024-03-13 23:15:39 +0100

    WIP

--- src/output/nixosConfigurations/default.nix
+++ src/output/nixosConfigurations/default.nix
@@ -130,41 +130,6 @@ builtins.foldl'
             home-manager.extraSpecialArgs = specialArgs;
             home-manager.sharedModules = metaModules ++ [
               nur.hmModules.nur
-              ({ pkgs, ... }: {
-                nix.package = pkgs.nixFlakes;
-                nix.extraOptions = "experimental-features = nix-command flakes";
-                nix.gc.automatic = true;
-                nix.gc.dates = "weekly";
-                nix.gc.options = "--delete-older-than 30d";
-                nix.settings.auto-optimise-store = true;
-                nix.settings.substituters = [
-                  "https://cache.nixos.org"
-                  "https://nix-community.cachix.org"
-                  "https://haras.cachix.org"
-                  "https://hyprland.cachix.org"
-                  "https://ai.cachix.org"
-                  "https://cuda-maintainers.cachix.org"
-                ];
-                nix.settings.trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  "haras.cachix.org-1:/HIo1JYqOIH1Nwk1EGXhuPPvDW0WekxIbY5CiXUZbYw="
-                  "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-                  "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
-                  "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-                ];
-                nix.settings.allowed-users = [
-                  "root"
-                  "@wheel"
-                ];
-                nix.settings.trusted-users = [
-                  "root"
-                ];
-
-                programs.home-manager.enable = true;
-
-                home.stateVersion = "23.11";
-              })
               lulezojne.homeManagerModules.default
               sops-nix.homeManagerModules.sops
               metaConfigModule
@@ -207,11 +172,47 @@ builtins.foldl'
                   };
                 in
                 {
+                  nix.package = pkgs.nixFlakes;
+                  nix.extraOptions = "experimental-features = nix-command flakes";
+                  nix.gc.automatic = true;
+                  nix.gc.dates = "weekly";
+                  nix.gc.options = "--delete-older-than 30d";
+                  nix.settings.auto-optimise-store = true;
+                  nix.settings.substituters = [
+                    "https://cache.nixos.org"
+                    "https://nix-community.cachix.org"
+                    "https://haras.cachix.org"
+                    "https://hyprland.cachix.org"
+                    "https://ai.cachix.org"
+                    "https://cuda-maintainers.cachix.org"
+                  ];
+                  nix.settings.trusted-public-keys = [
+                    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                    "haras.cachix.org-1:/HIo1JYqOIH1Nwk1EGXhuPPvDW0WekxIbY5CiXUZbYw="
+                    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                    "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+                    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+                  ];
+                  nix.settings.allowed-users = [
+                    "root"
+                    "@wheel"
+                  ];
+                  nix.settings.trusted-users = [
+                    "root"
+                  ];
+
+                  programs.home-manager.enable = true;
+
                   home.username = "${userName}";
                   home.homeDirectory = "/home/${userName}";
+
                   home.packages = [ rebuild rebuild-wip ];
+
                   sops.defaultSopsFile = "${self}/src/host/${hostName}/${userName}.sops.enc.yaml";
                   sops.age.keyFile = "/home/${userName}/.sops/secrets.age";
+
+                  home.stateVersion = "23.11";
                 });
           }
           else
