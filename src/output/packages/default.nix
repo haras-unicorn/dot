4ec9ba73289feefab3887a6e9f4a78b8033c50eb
haras <social@hrvojej.anonaddy.me>
2024-03-13 20:07:16 +0100

    WIP

--- src/output/packages/default.nix
+++ src/output/packages/default.nix
@@ -3,37 +3,33 @@
 , ...
 }:
 
-let
-  configs = flake-utils.lib.defaultSystems;
-
-  rzls = system:
-    with nixpkgs.legacyPackages.${system};
-    buildDotnetModule rec {
-      pname = "rzls";
-      version = "17.0.4";
-
-      src = fetchFromGitHub {
-        owner = "dotnet";
-        repo = "razor";
-        rev = "v${version}";
-        sha256 = "";
-      };
-
-      projectFile = "src/Razor/src/rzls/rzls.csproj";
-
-      meta = {
-        homepage = "https://asp.net";
-        description = "Compiler and tooling experience for Razor ASP.NET Core apps in Visual Studio, Visual Studio for Mac, and VS Code.";
-        license = lib.licenses.mit;
-      };
-    };
-
-in
 builtins.foldl'
   (apps: system: apps // {
     "${system}" = {
-      rzls = rzls system;
+      rzls =
+        with nixpkgs.legacyPackages.${system};
+        buildDotnetModule rec {
+          pname = "rzls";
+          version = "3.1.30";
+
+          src = fetchFromGitHub {
+            owner = "dotnet";
+            repo = "razor";
+            rev = "v${version}";
+            sha256 = "sha256-YX5LmiIzG2Xeno/Ys91cmrf41uK7CcV4HK2TnYEk1A8=";
+          };
+
+          nugetDeps = [ ];
+
+          projectFile = "src/Razor/src/rzls/rzls.csproj";
+
+          meta = {
+            homepage = "https://asp.net";
+            description = "Compiler and tooling experience for Razor ASP.NET Core apps in Visual Studio, Visual Studio for Mac, and VS Code.";
+            license = lib.licenses.mit;
+          };
+        };
     };
   })
   ({ })
-  configs
+  flake-utils.lib.defaultSystems
