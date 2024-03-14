7900918bd2e9799a2844e266291fd8c42427c697
haras <social@hrvojej.anonaddy.me>
2024-03-14 20:11:10 +0100

    WIP

--- src/module/home/code/default.nix
+++ src/module/home/code/default.nix
@@ -38,14 +38,19 @@
   programs.vscode.enableExtensionUpdateCheck = false;
   programs.vscode.enableUpdateCheck = false;
   programs.vscode.mutableExtensionsDir = false;
-  programs.vscode.extensions = builtins.map
-    (extension:
-      pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-        vsix = builtins.fetchurl
-          (if builtins.hasAttr "platforms" extension.src
-          then ({ inherit (extension.src) name; } // extension.src.platforms.${system})
-          else extension.src);
-        mktplcRef = { inherit (extension) name publisher version; };
-      })
-    (builtins.fromJSON (builtins.readFile ./extensions.json));
+  programs.vscode.extensions = builtins.filter
+    (extension: extension != null)
+    (builtins.map
+      (extension:
+        if builtins.hasAttr "platforms" extension.src && (! builtins.hasAttr "${system}" extension.src.platforms)
+        then null
+        else
+          (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+            vsix = builtins.fetchurl
+              (if builtins.hasAttr "platforms" extension.src
+              then ({ inherit (extension.src) name; } // extension.src.platforms.${system})
+              else extension.src);
+            mktplcRef = { inherit (extension) name publisher version; };
+          }))
+      (builtins.fromJSON (builtins.readFile ./extensions.json)));
 }
