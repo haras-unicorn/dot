400d3eea768681bdd53b1725f8ddd1b6b1b7ef75
haras <social@hrvojej.anonaddy.me>
2024-03-14 14:56:57 +0100

    WIP

--- src/module/home/llama-cpp/default.nix
+++ src/module/home/llama-cpp/default.nix
@@ -7,12 +7,7 @@ let
     name = "write";
     runtimeInputs = [ llama-cpp ];
     text = ''
-      prompt="$1"
-      if [[ "$prompt" == "" ]]; then
-        printf "I need a prompt to write.\n"
-      fi
-
-      command="llama --prompt '$prompt' --no-display-prompt --log-disable"
+      command="llama --prompt '$@' --no-display-prompt --log-disable"
       while IFS= read -r line; do
         command+=" $line"
       done < "${config.home.homeDirectory}/write/llama.options"
