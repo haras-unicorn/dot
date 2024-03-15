9ab28b77ee2ccc49d1d9726ac8d1236a7cfa2260
haras <social@haras.anonaddy.me>
2024-03-15 11:23:56 +0100

    WIP

--- src/module/system/network/default.nix
+++ src/module/system/network/default.nix
@@ -4,4 +4,6 @@
   networking.nftables.enable = true;
   networking.firewall.enable = true;
   networking.networkmanager.enable = true;
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.networkmanager.dns = "none";
 }
