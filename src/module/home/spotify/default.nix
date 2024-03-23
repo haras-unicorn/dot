c5712a3bded6d87b632c456fb440a2f2d43d0ab4
haras <social@hrvojej.anonaddy.me>
2024-03-23 21:29:29 +0100

    WIP

--- src/module/home/spotify/default.nix
+++ src/module/home/spotify/default.nix
@@ -1,8 +1,22 @@
 { pkgs, ... }:
 
+let
+  # TODO: like with chromium?
+  spotify = pkgs.symlinkJoin {
+    name = "spotify";
+    paths = [ pkgs.spotify ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify \
+        --append-flags --enable-features=WebRTCPipeWireCapturer \
+        --append-flags --enable-features=UseOzonePlatform \
+        --append-flags --ozone-platform-hint=auto
+    '';
+  };
+in
 {
-  home.packages = with pkgs; [
+  home.packages = [
     spotify
-    psst
+    pkgs.psst
   ];
 }
