94cb02f420d17ef2a2b7e01cc60db63c8cdc55c2
haras <social@hrvojej.anonaddy.me>
2024-05-02 15:06:33 +0200

    WIP

---
+++ src/lib/modules.nix
@@ -0,0 +1,123 @@
+# dot modules come in two forms: function and object
+
+# Function modules take input args from nixpkgs.lib.nixosSystem and home-manager module parameters
+# Object modules are like function modules but don't take any parameters
+
+# The schema closely follows nixpkgs.lib.nixosSystem module schema
+# Each module can import other modules and can declare options directly or be separated into config and options parts
+
+# Each module config has a system and home part
+# The system part is applied to nixpkgs.lib.nixosSystem
+# The home part is applied to home manager
+
+# The home part also has a shared part and a per-user part
+# The shared part is applied to each user via home-manager.sharedModules
+# The per-user part is applied to individual users (not yet implemented)
+
+# Here is a kitchen sink example:
+# { self, lib, config, ... }:
+# with lib;
+# let
+#   cfg = config.desktopEnvironment;
+#   vars = strings.concatStringsSep
+#     "\n"
+#     (builtins.map
+#       (name: "env = ${name}, ${builtins.toString cfg.sessionVariables."${name}"}")
+#       (builtins.attrNames cfg.sessionVariables));
+# in
+# {
+#   imports = [ "${self}/src/modules/waybar" ];
+#   options.desktopEnvironment = {
+#     sessionVariables = mkOption {
+#       type = with types; lazyAttrsOf (oneOf [ str path int float ]);
+#       default = { };
+#       example = { EDITOR = "hx"; };
+#       description = ''
+#         Environment variables to set on session start with Hyprland.
+#       '';
+#     };
+#   };
+#   config = {
+#     system = {
+#       programs.hyprland.enable = true;
+#       programs.hyprland.xwayland.enable = true;
+#     };
+#     home.shared = {
+#       wayland.windowManager.hyprland.enable = true;
+#       wayland.windowManager.hyprland.xwayland.enable = true;
+#       wayland.windowManager.hyprland.extraConfig = ''
+#         ${vars}
+#       '';
+#     };
+#   };
+# }
+
+# This module aims to construct modules digestible by nixpkgs.lib.nixosSystem and
+# home-manager from dot modules
+
+let
+  mkDotObject = inputs: dotModule: if builtins.isFunction dotModule then (dotModule inputs) else dotModule;
+  mkImports = mkModule: inputs: dotObject: builtins.map
+    (importName: (mkModule (import importName)) inputs)
+    (if builtins.hasAttr "imports" dotObject then dotObject.imports else [ ]);
+  mkOptions = inputs: dotObject: if builtins.hasAttr "options" dotObject then dotObject.options else { };
+  mkConfig = { lib, ... }: path: dotObject:
+    if builtins.hasAttr "config" dotObject then
+      let
+        configObject = dotObject.config;
+      in
+      if lib.hasAttrByPath path configObject then
+        lib.getAttrByPath path configObject
+      else { }
+    else dotObject;
+  concatModules = modules: builtins.foldl'
+    (acc: next: { options = acc.options // next.options; config = acc.config // next.config; })
+    { }
+    modules;
+in
+rec {
+  mkSystemModule = (dotModule:
+    inputs:
+    let
+      dotObject = mkDotObject inputs dotModule;
+      imports = mkImports mkSystemModule inputs dotObject;
+      options = mkOptions inputs dotObject;
+      config = mkConfig inputs [ "system" ] dotObject;
+    in
+    concatModules (imports ++ [{ inherit options config; }]));
+
+  mkHomeSharedModule = (dotModule:
+    inputs:
+    let
+      dotObject = mkDotObject inputs dotModule;
+      imports = mkImports mkSystemModule inputs dotObject;
+      options = mkOptions inputs dotObject;
+      config = mkConfig inputs [ "home" "shared" ] dotObject;
+    in
+    concatModules (imports ++ [{ inherit options config; }]));
+
+  definedUsers = (dotModule:
+    { lib, ... } @inputs:
+    let
+      dotObject = mkDotObject inputs dotModule;
+      imports = mkImports definedUsers inputs dotObject;
+      config = mkConfig inputs [ "home" ] dotObject;
+    in
+    lib.lists.unique (
+      lib.lists.flatten (
+        imports
+        ++ (builtins.filter
+          (user: user != "shared")
+          (builtins.attrNames config))
+      )));
+
+  mkHomeUserModule = (user: dotModule:
+    inputs:
+    let
+      dotObject = mkDotObject inputs dotModule;
+      imports = mkImports mkSystemModule inputs dotObject;
+      options = mkOptions inputs dotObject;
+      config = mkConfig inputs [ "home" user ] dotObject;
+    in
+    concatModules (imports ++ [{ inherit options config; }]));
+}
