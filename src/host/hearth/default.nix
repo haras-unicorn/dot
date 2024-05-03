66475da5ac41a032b6a4c11b5943e4003a2d52a0
haras <social@hrvojej.anonaddy.me>
2024-05-04 01:25:05 +0200

    WIP

--- src/host/hearth/default.nix
+++ src/host/hearth/default.nix
@@ -70,7 +70,7 @@
       ];
 
       gpg = { package = pkgs.pinentry-qt; bin = "pinentry-qt"; flavor = "qt"; };
-      shell = { package = pkgs.nushell; bin = "nu"; };
+      shell = { package = pkgs.nushell.override { additionalFeatures = (p: p ++ [ "dataframe" ]); }; bin = "nu"; };
       editor = { package = pkgs.helix; bin = "hx"; };
       visual = { package = pkgs.vscode; bin = "code"; };
       terminal = { package = pkgs.kitty; bin = "kitty"; };
