f674007e04d7f83232e8f9fa08d87122052a3eee
haras <social@hrvojej.anonaddy.me>
2024-05-04 01:19:31 +0200

    WIP

--- src/module/bluetooth/default.nix
+++ src/module/bluetooth/default.nix
@@ -2,9 +2,7 @@
 
 {
   system = {
-    hardware.bluetooth.enable = true; # enables support for Bluetooth
-    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
+    hardware.bluetooth.enable = true;
     services.blueman.enable = true;
   };
 }
