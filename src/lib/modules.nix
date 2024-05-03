5959f2e032ffb2fc1842c8f8e042c0f6454ba03a
haras <social@hrvojej.anonaddy.me>
2024-05-03 20:55:44 +0200

    WIP

--- src/lib/modules.nix
+++ src/lib/modules.nix
@@ -119,7 +119,7 @@ rec {
       ));
 
   # NOTE: if pkgs here not demanded other modules don't get access...
-  mkSystemModule = dotModule: { pkgs, config, ... } @specialArgs:
+  mkSystemModule = dotModule: { pkgs, ... } @specialArgs:
     let
       dotObject = mkDotObject specialArgs dotModule;
       imports = mkImports mkSystemModule specialArgs dotObject;
