e1b33a7084e4549630c407a34ab4e4f74e47590e
haras <social@hrvojej.anonaddy.me>
2024-05-04 13:06:51 +0200

    WIP

--- src/module/helix/default.nix
+++ src/module/helix/default.nix
@@ -31,7 +31,67 @@ in
     programs.helix.languages = {
       language-server = {
         nil = { command = "${pkgs.nil}/bin/nil"; };
+        taplo = { command = "${pkgs.taplo}/bin/taplo"; args = [ "server" ]; };
+        yaml-language-server = {
+          command = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+          args = [ "--stdio" ];
+        };
+        vscode-json-language-server = {
+          command = "${pkgs.nodePackages.vscode-json-languageserver}/bin/vscode-json-language-server";
+          args = [ "--stdio" ];
+          config = {
+            provideFormatter = true;
+            format = { enable = true; };
+            json = { validate = { enable = true; }; };
+          };
+        };
+        marksman = { command = "${pkgs.marksman}/bin/marksman"; args = [ "server" ]; };
       };
+
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter = { command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"; };
+          language-servers = [ "nil" ];
+        }
+        {
+          name = "toml";
+          auto-format = true;
+          language-servers = [ "taplo" ];
+        }
+        {
+          name = "yaml";
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.nodePackages.prettier}/bin/prettier";
+            args = [ "--parser" "yaml" ];
+          };
+          language-servers = [ "yaml-language-server" ];
+        }
+        {
+          name = "json";
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.nodePackages.prettier}/bin/prettier";
+            args = [ "--parser" "json" ];
+          };
+          language-servers = [ "vscode-json-language-server" ];
+        }
+        {
+          name = "markdown";
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.nodePackages.prettier}/bin/prettier";
+            args = [ "--parser" "json" ];
+          };
+        }
+        {
+          name = "xml";
+          auto-format = true;
+          formatter = { command = "${pkgs.html-tidy}/bin/tidy"; args = [ "-xml" "-i" ]; };
+        }
+      ];
     };
   };
 }
