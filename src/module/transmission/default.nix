d49c99efc5e26bc0bbbbefa0c161044c08b3c5f0
haras <social@hrvojej.anonaddy.me>
2024-05-19 13:25:56 +0200

    WIP

--- src/module/transmission/default.nix
+++ src/module/transmission/default.nix
@@ -1,10 +1,18 @@
-{ ... }:
-
-# FIXME: per user?
+{ config, ... }:
 
 {
   system = {
     services.transmission.enable = true;
     services.transmission.openPeerPorts = true;
+  };
+
+  home.shared = {
+    xdg.desktopEntries = {
+      syncthing = {
+        name = "Transmission";
+        exec = "${config.dot.browser.package}/bin/${config.dot.browser.bin} localhost:9091";
+        terminal = false;
+      };
+    };
   };
 }
