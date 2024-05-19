17a2b8b004a77104f9933ff71cb9d1bdafba598e
haras <social@hrvojej.anonaddy.me>
2024-05-19 13:41:46 +0200

    WIP

--- src/module/transmission/default.nix
+++ src/module/transmission/default.nix
@@ -1,21 +1,29 @@
-{ config, ... }:
+{
+  # config, 
+  pkgs
+, ...
+}:
 
 # NOTE: peer port is 51413
 # NOTE: webui port is 9091
 
 {
   system = {
-    services.transmission.enable = true;
-    services.transmission.openPeerPorts = true;
+    # services.transmission.enable = true;
+    # services.transmission.openPeerPorts = true;
   };
 
   home.shared = {
-    xdg.desktopEntries = {
-      transmission = {
-        name = "Transmission";
-        exec = "${config.dot.browser.package}/bin/${config.dot.browser.bin} localhost:9091";
-        terminal = false;
-      };
-    };
+    # xdg.desktopEntries = {
+    #   transmission = {
+    #     name = "Transmission";
+    #     exec = "${config.dot.browser.package}/bin/${config.dot.browser.bin} localhost:9091";
+    #     terminal = false;
+    #   };
+    # };
+
+    home.packages = [
+      pkgs.transmission_4-gtk
+    ];
   };
 }
