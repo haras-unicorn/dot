ee85351c23ad67d615a4ae309275fd138d0eee70
haras <social@hrvojej.anonaddy.me>
2024-05-24 21:24:20 +0200

    WIP

--- src/distro/console/default.nix
+++ src/distro/console/default.nix
@@ -129,6 +129,7 @@ in
         nixos-generators # NOTE: collection of generators to create nixos images
         dos2unix # NOTE: convert Windows file endings to Unix
         unixtools.xxd # NOTE: make hexdump
+        github-copilot-cli # Copilot CLI
       ];
     };
   };
