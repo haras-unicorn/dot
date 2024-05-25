4f7fe0bfbbb759f99e4911e1a3c227f3ef64e865
haras <social@haras.anonaddy.me>
2024-05-25 12:17:51 +0200

    WIP

--- src/output/nixosConfigurations/default.nix
+++ src/output/nixosConfigurations/default.nix
@@ -178,7 +178,7 @@ builtins.foldl'
         (dot.modules.mkSystemModule dotModule)
         home-manager.nixosModules.home-manager
         ({
-          home-manager.backupFileExtension = "bkp";
+          home-manager.backupFileExtension = "backup";
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = specialArgs;
           home-manager.sharedModules = [
