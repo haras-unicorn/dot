0b13e4c9f1063d521ff51f270008f3229de7d27b
haras <social@hrvojej.anonaddy.me>
2024-07-06 11:09:09 +0200

    WIP

--- src/module/gtklock/default.nix
+++ src/module/gtklock/default.nix
@@ -3,6 +3,18 @@
 {
   system = {
     security.pam.services.gtklock = { };
+    services.systemd-lock-handler.enable = true;
+
+    systemd.user.services.gtklock = {
+      description = "GTK Lock Service";
+      wantedBy = [ "lock.target" "sleep.target" ];
+      before = [ "sleep.target" ];
+      script = "${pkgs.gtklock}/bin/gtklock";
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+    };
   };
 
   home.shared = {
