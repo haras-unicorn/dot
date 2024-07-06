1ca69b6106844be4f6c0ebc48c7e56ed423adc19
haras <social@hrvojej.anonaddy.me>
2024-07-06 19:15:49 +0200

    WIP

--- src/module/swayidle/default.nix
+++ src/module/swayidle/default.nix
@@ -5,11 +5,11 @@
     services.swayidle.enable = true;
     services.swayidle.timeouts = [
       {
-        timeout = 60;
+        timeout = 60 * 5;
         command = "${pkgs.systemd}/bin/loginctl lock-session";
       }
       {
-        timeout = 600;
+        timeout = 60 * 60;
         command = "${pkgs.systemd}/bin/systemctl suspend-then-hibernate";
       }
     ];
