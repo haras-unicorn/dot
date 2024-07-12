376da0821f8cde155d4ac791b033fec1f436124a
haras <social@hrvojej.anonaddy.me>
2024-07-12 12:49:01 +0200

    WIP

--- src/module/cloud/default.nix
+++ src/module/cloud/default.nix
@@ -45,23 +45,22 @@ in
 {
   home.shared = {
     home.packages = with pkgs; [
-      # (azure-cli.withExtensions [
-      #   (mkAzExtension rec {
-      #     pname = "ssh";
-      #     version = "2.0.4";
-      #     url = "https://azcliprod.blob.core.windows.net/cli-extensions/ssh-${version}-py3-none-any.whl";
-      #     sha256 = "0d4hna7s5yrycfzvf86p41qi5j2xll2zz7sqval17zv1mq64q5gr";
-      #     description = "SSH into Azure VMs using RBAC and AAD OpenSSH Certificates";
-      #     buildInputs = [
-      #       oschmod
-      #     ];
-      #     propagatedBuildInputs = (with pkgs.python311Packages; [
-      #       oschmod
-      #       oras
-      #     ]);
-      #   })
-      # ])
-      (python311.withPackages (_: [ oschmod ]))
+      (azure-cli.withExtensions [
+        (mkAzExtension rec {
+          pname = "ssh";
+          version = "2.0.4";
+          url = "https://azcliprod.blob.core.windows.net/cli-extensions/ssh-${version}-py3-none-any.whl";
+          sha256 = "0d4hna7s5yrycfzvf86p41qi5j2xll2zz7sqval17zv1mq64q5gr";
+          description = "SSH into Azure VMs using RBAC and AAD OpenSSH Certificates";
+          buildInputs = [
+            oschmod
+          ];
+          propagatedBuildInputs = (with pkgs.python311Packages; [
+            oschmod
+            oras
+          ]);
+        })
+      ])
     ];
 
     home.file.".azure/config".source = ./azure-config;
