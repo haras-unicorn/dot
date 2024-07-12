05be2e7b563cc5865df8a8fc445d3a80286872eb
haras <social@hrvojej.anonaddy.me>
2024-07-12 11:42:17 +0200

    WIP

--- src/module/cloud/default.nix
+++ src/module/cloud/default.nix
@@ -5,7 +5,15 @@
 {
   home.shared = {
     home.packages = with pkgs; [
-      azure-cli
+      (azure-cli.withExtensions [
+        (pkgs.mkAzExtension rec {
+          pname = "ssh";
+          version = "2.0.4";
+          url = "https://azcliprod.blob.core.windows.net/cli-extensions/ssh-${version}-py3-none-any.whl";
+          sha256 = "";
+          description = "SSH into Azure VMs using RBAC and AAD OpenSSH Certificates";
+        })
+      ])
     ];
 
     home.file.".azure/config".source = ./azure-config;
