b6b695b034e39d157d51fa6aa65cffb34308d41a
haras <social@hrvojej.anonaddy.me>
2024-07-22 18:53:40 +0200

    WIP

--- src/module/wayland/default.nix
+++ src/module/wayland/default.nix
@@ -49,6 +49,7 @@ in
         NIXOS_XDG_OPEN_USE_PORTAL = "1";
         GDK_BACKEND = "wayland,x11";
         CLUTTER_BACKEND = "wayland";
+        XWAYLAND_NO_GLAMOR = "1";
         _JAVA_AWT_WM_NONREPARENTING = "1";
       };
 
