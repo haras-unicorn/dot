16eb0f7d4e8376c8adfc7659cbe8d3cabd187d49
haras <social@hrvojej.anonaddy.me>
2024-07-31 18:58:16 +0200

    WIP

--- src/module/ddns/default.nix
+++ src/module/ddns/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 # NOTE: https://github.com/qdm12/ddns-updater/blob/master/docs/duckdns.md -> /var/lib/ddns-updater/config.json
 
@@ -7,6 +7,18 @@
     services.ddns-updater.enable = true;
     services.ddns-updater.environment = {
       CONFIG_FILEPATH = "/etc/ddns-updater.json";
+    };
+  };
+
+  home = {
+    shared = {
+      xdg.desktopEntries = {
+        ddns-updater = {
+          name = "DDNS Updater";
+          exec = "${config.dot.browser.package}/bin/${config.dot.browser.bin} --new-window localhost:8000";
+          terminal = false;
+        };
+      };
     };
   };
 }
