5965c768a000a8cae0e6ee5d19baf68a6641093a
haras <social@hrvojej.anonaddy.me>
2024-07-31 18:56:29 +0200

    WIP

--- src/module/ddns/default.nix
+++ src/module/ddns/default.nix
@@ -1,9 +1,12 @@
 { ... }:
 
-# NOTE: https://github.com/qdm12/ddns-updater/blob/master/docs/duckdns.md -> $XDG_STATE_HOME/ddns-updater/config.json
+# NOTE: https://github.com/qdm12/ddns-updater/blob/master/docs/duckdns.md -> /var/lib/ddns-updater/config.json
 
 {
   system = {
     services.ddns-updater.enable = true;
+    services.ddns-updater.environment = {
+      CONFIG_FILEPATH = "/etc/ddns-updater.json";
+    };
   };
 }
