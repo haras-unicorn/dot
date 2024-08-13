316b838cd4d1f237b20d8fe053a5dfed85e028ea
haras <social@hrvojej.anonaddy.me>
2024-08-13 20:39:21 +0200

    WIP

--- src/module/pinokio/default.nix
+++ src/module/pinokio/default.nix
@@ -6,6 +6,14 @@
       home.packages = with pkgs; [
         pinokio
       ];
+
+      xdg.desktopEntries = {
+        pinokio = {
+          name = "Pinokio";
+          exec = "${pkgs.pinokio}/bin/pinokio";
+          terminal = false;
+        };
+      };
     };
   };
 }
