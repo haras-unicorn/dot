ec4a94ecf1d31da719c38245df82d42cdad8e961
haras <social@hrvojej.anonaddy.me>
2024-08-13 22:08:25 +0200

    WIP

--- src/module/pinokio/default.nix
+++ src/module/pinokio/default.nix
@@ -4,15 +4,15 @@ let
   pinokio = pkgs.pinokio;
 in
 {
-  system = {
-    programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-      cudaPackages.cudnn
-      cudaPackages.cuda_nvrtc
-      cudaPackages.nccl
-      cudaPackages.cuda_cudart
-    ];
-  };
+  # system = {
+  #   programs.nix-ld.enable = true;
+  #   programs.nix-ld.libraries = with pkgs; [
+  #     cudaPackages.cudnn
+  #     cudaPackages.cuda_nvrtc
+  #     cudaPackages.nccl
+  #     cudaPackages.cuda_cudart
+  #   ];
+  # };
   home = {
     shared = {
       home.packages = [
