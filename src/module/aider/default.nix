d42492a6f78be0719f5b50f77703ce0649085603
haras <social@hrvojej.anonaddy.me>
2024-09-15 19:44:40 +0200

    WIP

--- src/module/aider/default.nix
+++ src/module/aider/default.nix
@@ -3,7 +3,7 @@
 let
   aider = pkgs.writeShellApplication {
     name = "aider";
-    runtime-inputs = [ pkgs.aider-chat ];
+    runtimeInputs = [ pkgs.aider-chat ];
     text = ''
       if [ -f ${config.home.homeDirectory}/.openai/api.key ]; then
         export OPENAI_API_KEY=$(cat ${config.home.homeDirectory}/.openai/api.key)
