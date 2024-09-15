4cecf1a0215e8482d9f5f2381edf7425fd043d6f
haras <social@hrvojej.anonaddy.me>
2024-09-15 19:44:08 +0200

    WIP

--- src/module/aider/default.nix
+++ src/module/aider/default.nix
@@ -1,10 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  aider = pkgs.writeShellApplication {
+    name = "aider";
+    runtime-inputs = [ pkgs.aider-chat ];
+    text = ''
+      if [ -f ${config.home.homeDirectory}/.openai/api.key ]; then
+        export OPENAI_API_KEY=$(cat ${config.home.homeDirectory}/.openai/api.key)
+        export AIDER_MINI=1
+      fi
+
+      aider $@
+    '';
+  };
+in
 {
   home = {
     shared = {
-      home.packages = with pkgs; [
-        aider-chat
+      home.packages = [
+        aider
       ];
     };
   };
