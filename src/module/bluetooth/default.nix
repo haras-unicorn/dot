c6f1063e3eed59e410e12a487f6e17cef44ba595
haras <social@hrvojej.anonaddy.me>
2024-09-25 18:09:49 +0200

    WIP

--- src/module/bluetooth/default.nix
+++ src/module/bluetooth/default.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   system = {
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+  };
+
+  shared = {
+    dot = {
+      desktopEnvironment.sessionStartup = [
+        "${pkgs.blueman}/bin/blueman-applet"
+      ];
+    };
   };
 }
