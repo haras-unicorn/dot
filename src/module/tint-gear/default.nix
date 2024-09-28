bb71706c1c5f5a734984aae04326f06b87a6aed3
haras <social@hrvojej.anonaddy.me>
2024-09-28 18:19:47 +0200

    WIP

--- src/module/tint-gear/default.nix
+++ src/module/tint-gear/default.nix
@@ -1,15 +1,20 @@
 { tint-gear, system, config, ... }:
 
+let
+  colors = tint-gear.lib.colors {
+    imagePath = config.dot.wallpaper;
+  };
+in
 {
   shared = {
     dot = {
-      colors = tint-gear.lib.colors {
-        imagePath = config.dot.wallpaper;
-      };
+      inherit colors;
     };
   };
 
   home.shared = {
     home.packages = [ tint-gear.packages."${system}".default ];
+
+    xdg.configFile."tint-gear/colors.json".text = builtins.toJSON colors;
   };
 }
