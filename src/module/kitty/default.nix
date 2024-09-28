00e50a2fbc845aa8e43913da94f0169f492a5aa9
haras <social@hrvojej.anonaddy.me>
2024-09-28 15:11:35 +0200

    WIP

--- src/module/kitty/default.nix
+++ src/module/kitty/default.nix
@@ -12,44 +12,13 @@ let
   shell = config.dot.shell;
   editor = config.dot.editor;
   font = config.dot.font;
+
+  bootstrap = config.dot.colors.bootstrap;
+  terminal = config.dot.colors.terminal;
 in
 {
   config = {
     home.shared = {
-      programs.lulezojne.config.plop = [
-        {
-          template = ''
-            background_opacity 0.5
-
-            foreground {{ hex (set-lightness ansi.main.bright_white 0.9) }}
-            background {{ hex (set-lightness ansi.main.black 0.1) }}
-
-            color0     {{ hex ansi.main.black }}
-            color1     {{ hex (set-lightness ansi.main.red 0.7) }}
-            color2     {{ hex (set-lightness ansi.main.green 0.7) }}
-            color3     {{ hex (set-lightness ansi.main.yellow 0.7) }}
-            color4     {{ hex (set-lightness ansi.main.blue 0.7) }}
-            color5     {{ hex (set-lightness ansi.main.magenta 0.7) }}
-            color6     {{ hex (set-lightness ansi.main.cyan 0.7) }}
-            color7     {{ hex (set-lightness ansi.main.white 0.7) }}
-
-            color8     {{ hex (set-lightness ansi.main.bright_black 0.5) }}
-            color9     {{ hex (set-lightness ansi.main.bright_red 0.9) }}
-            color10    {{ hex (set-lightness ansi.main.bright_green 0.9) }}
-            color11    {{ hex (set-lightness ansi.main.bright_yellow 0.9) }}
-            color12    {{ hex (set-lightness ansi.main.bright_blue 0.9) }}
-            color13    {{ hex (set-lightness ansi.main.bright_magenta 0.9) }}
-            color14    {{ hex (set-lightness ansi.main.bright_cyan 0.9) }}
-            color15    {{ hex ansi.main.bright_white }}
-          '';
-          "in" = "${config.xdg.configHome}/kitty/colors.conf";
-          "then" = {
-            command = "pkill";
-            args = [ "--signal" "SIGUSR1" "kitty" ];
-          };
-        }
-      ];
-
       programs.kitty.enable = true;
       programs.kitty.package =
         (p: yes: no: lib.mkMerge [
@@ -69,7 +38,28 @@ in
         bold_italic_font ${font.nerd.name}
         font_size        ${builtins.toString font.size.medium}
 
-        include colors.conf
+        background_opacity 0.5
+
+        foreground ${bootstrap.text}
+        background ${bootstrap.background}
+
+        color0     ${terminal.black}
+        color1     ${terminal.red}
+        color2     ${terminal.green}
+        color3     ${terminal.yellow}
+        color4     ${terminal.blue}
+        color5     ${terminal.magenta}
+        color6     ${terminal.cyan}
+        color7     ${terminal.white}
+
+        color8     ${terminal.brightBlack}
+        color9     ${terminal.brightRed}
+        color10    ${terminal.brightGreen}
+        color11    ${terminal.brightYellow}
+        color12    ${terminal.brightBlue}
+        color13    ${terminal.brightMagenta}
+        color14    ${terminal.brightCyan}
+        color15    ${terminal.brightWhite}
 
         shell ${shell.package}/bin/${shell.bin}
         editor  ${editor.package}/bin/${editor.bin}
