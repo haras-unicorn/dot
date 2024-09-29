b72f353ffce9c8f50417c8fe0c730b95f30ac87b
haras <social@hrvojej.anonaddy.me>
2024-09-29 18:58:03 +0200

    WIP

--- src/module/alarm/default.nix
+++ src/module/alarm/default.nix
@@ -3,12 +3,15 @@
 # TODO: figure out why the sound doesn't work
 
 {
-  shared = {
-    dot = {
-      desktopEnvironment.sessionStartup = [
-        "${pkgs.alarm-clock-applet}/bin/alarm-clock-applet"
-      ];
-    };
+  shared.dot = {
+    desktopEnvironment.sessionStartup = [
+      "${pkgs.alarm-clock-applet}/bin/alarm-clock-applet"
+    ];
+    desktopEnvironment.windowrules = [{
+      rule = "float";
+      selector = "class";
+      arg = "alarm-clock-applet";
+    }];
   };
 
   home.shared = {
