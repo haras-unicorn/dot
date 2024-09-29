60b30bc75b3d9b908698541fa9cd418e024fda9b
haras <social@hrvojej.anonaddy.me>
2024-09-29 19:24:27 +0200

    WIP

--- src/module/zed/default.nix
+++ src/module/zed/default.nix
@@ -21,70 +21,70 @@ in
         name = "colors";
         appearance = if config.dot.colors.isLightTheme then "light" else "dark";
         style = {
-          border = bootstrap.primary.normal;
-          "border.variant" = bootstrap.secondary.normal;
-          background = bootstrap.background.normal;
-          "panel.background" = bootstrap.background.normal;
-          "tab_bar.background" = bootstrap.background.normal;
-          "tab.active_background" = bootstrap.background.normal;
-          "tab.inactive_background" = bootstrap.background.alternate;
-          "title_bar.background" = bootstrap.background.normal;
-          "toolbar.background" = bootstrap.background.normal;
-          "terminal.background" = bootstrap.background.normal;
-          "surface.background" = bootstrap.background.normal;
-          "scrollbar.track.border" = bootstrap.secondary.normal;
-          "status_bar.background" = bootstrap.background.normal;
-          "editor.active_line.background" = bootstrap.background.alternate;
-          "editor.background" = bootstrap.background.normal;
-          "editor.foreground" = bootstrap.text.normal;
-          "editor.line_number" = bootstrap.text.alternate;
-          "editor.gutter.background" = bootstrap.background.normal;
-          "ghost_element.selected" = bootstrap.selection.normal;
-          "ghost_element.background" = bootstrap.selection.alternate;
-          "ghost_element.hover" = bootstrap.selection.normal;
+          border = bootstrap.primary.normal.hex;
+          "border.variant" = bootstrap.secondary.normal.hex;
+          background = bootstrap.background.normal.hex;
+          "panel.background" = bootstrap.background.normal.hex;
+          "tab_bar.background" = bootstrap.background.normal.hex;
+          "tab.active_background" = bootstrap.background.normal.hex;
+          "tab.inactive_background" = bootstrap.background.alternate.hex;
+          "title_bar.background" = bootstrap.background.normal.hex;
+          "toolbar.background" = bootstrap.background.normal.hex;
+          "terminal.background" = bootstrap.background.normal.hex;
+          "surface.background" = bootstrap.background.normal.hex;
+          "scrollbar.track.border" = bootstrap.secondary.normal.hex;
+          "status_bar.background" = bootstrap.background.normal.hex;
+          "editor.active_line.background" = bootstrap.background.alternate.hex;
+          "editor.background" = bootstrap.background.normal.hex;
+          "editor.foreground" = bootstrap.text.normal.hex;
+          "editor.line_number" = bootstrap.text.alternate.hex;
+          "editor.gutter.background" = bootstrap.background.normal.hex;
+          "ghost_element.selected" = bootstrap.selection.normal.hex;
+          "ghost_element.background" = bootstrap.selection.alternate.hex;
+          "ghost_element.hover" = bootstrap.selection.normal.hex;
           syntax = {
             constant = {
-              color = terminal.yellow.normal;
+              color = terminal.yellow.normal.hex;
               font_style = null;
               font_weight = null;
             };
             string = {
-              color = terminal.green.normal;
+              color = terminal.green.normal.hex;
               font_style = null;
               font_weight = null;
             };
             comment = {
-              color = terminal.brightBlack.normal;
+              color = terminal.brightBlack.normal.hex;
               font_style = null;
               font_weight = null;
             };
             keyword = {
-              color = terminal.magenta.normal;
+              color = terminal.magenta.normal.hex;
               font_style = null;
               font_weight = null;
             };
             parameter = {
-              color = terminal.blue.normal;
+              color = terminal.blue.normal.hex;
               font_style = null;
               font_weight = null;
             };
             punctuation = {
-              color = bootstrap.text.normal;
+              color = bootstrap.text.normal.hex;
               font_style = null;
               font_weight = null;
             };
             property = {
-              color = terminal.cyan.normal;
+              color = terminal.cyan.normal.hex;
               font_style = null;
               font_weight = null;
             };
             function = {
-              color = terminal.brightBlue.normal;
+              color = terminal.brightBlue.normal.hex;
               font_style = null;
               font_weight = null;
             };
             number = {
-              color = terminal.red.normal;
+              color = terminal.red.normal.hex;
               font_style = null;
               font_weight = null;
             };
