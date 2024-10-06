57d194a02991623668d5c4693b74ad244e88f585
haras <social@haras.anonaddy.me>
2024-10-06 22:33:58 +0200

    WIP

--- src/module/emote/default.nix
+++ src/module/emote/default.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 
-# FIXME: fix not actually typing stuff in
-
 {
   shared = {
     dot = {
@@ -9,16 +7,20 @@
         {
           mods = [ "super" ];
           key = "e";
-          # command = "${pkgs.emote}/bin/emote";
           command = "${pkgs.smile}/bin/smile";
         }
       ];
+
+      desktopEnvironment.windowrules = [{
+        rule = "float";
+        selector = "class";
+        arg = "it.mijorus.smile";
+      }];
     };
   };
 
   home.shared = {
     home.packages = with pkgs; [
-      # emote
       smile
     ];
   };
