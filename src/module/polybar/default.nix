b71d9fe1c9060250c4c63fa3ab550e78d273468c
haras <social@haras.anonaddy.me>
2024-10-07 15:37:48 +0200

    WIP

--- src/module/polybar/default.nix
+++ src/module/polybar/default.nix
@@ -20,7 +20,7 @@ in
     services.polybar.config = ./config.ini;
     services.polybar.package = package;
     services.polybar.script = "${package}/bin/polybar top &>/dev/null & disown %-";
-    services.polybar.settings = {
+    services.polybar.settings = rec {
       nix = {
         background = bootstrap.background.normal.hex;
         text = bootstrap.text.normal.hex;
@@ -34,6 +34,24 @@ in
         font = (builtins.toString config.dot.font.sans.name)
           + ":size="
           + (builtins.toString config.dot.font.size.large);
+      };
+      "module/battery" = {
+        format-charging = "%{F${nix.accent}} %percentage%%%{F-}";
+        format-discharging = "%{F${nix.text-alternate}}%icon% %percentage%%%{F-}";
+        label-full = "%{F${nix.text-alternate}} %percentage%%%{F-}";
+      };
+      "module/pulseaudio" = {
+        format-volume = "%{F${nix.text-alternate}}%percentage%% {icon}%{F-}";
+      };
+      "module/temperature" = {
+        format = "%{F${nix.text-alternate}}%temperature-c%°C%{F-}";
+        format-warn = "%{F${nix.accent}}%temperature-c%°C%{F-}";
+      };
+      "module/memory" = {
+        format = "%{F${nix.text-alternate}}󰍛 %used%%{F-}";
+      };
+      "module/cpu" = {
+        format = "%{F${nix.text-alternate}}󰻠 %usage%%%{F-}";
       };
     };
   };
