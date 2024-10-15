60f5e1fc57e43c9a0f91acf8f36fb6557e508203
haras <social@haras.anonaddy.me>
2024-10-15 23:09:28 +0200

    WIP

--- src/module/cloud/default.nix
+++ src/module/cloud/default.nix
@@ -1,15 +1,18 @@
-{ pkgs, ... }:
+{
+  # pkgs,
+  ...
+}:
 
-# FIXME: azure ssh extension can't find oschmod
+# FIXME: azure-cli: fabric: ModuleNotFoundError: No module named 'nacl'
 
 {
   home.shared = {
-    home.packages = with pkgs; [
-      (azure-cli.withExtensions (with pkgs; [
-        azure-cli-extensions.ssh
-      ]))
-    ];
+    # home.packages = with pkgs; [
+    #   (azure-cli.withExtensions (with pkgs; [
+    #     azure-cli-extensions.ssh
+    #   ]))
+    # ];
 
-    home.file.".azure/config".source = ./azure-config;
+    # home.file.".azure/config".source = ./azure-config;
   };
 }
