35660bf35bdeb080d3970bd71e638316bee56afd
haras <social@haras.anonaddy.me>
2024-11-01 10:59:17 +0100

    WIP

--- src/module/comfyui/default.nix
+++ src/module/comfyui/default.nix
@@ -1,11 +1,19 @@
 { nix-comfyui, pkgs, config, ... }:
 
-# TODO: pick cuda/rocm based on cudaSupport
-
 let
+  packageName =
+    if config.nixpkgs.config.cudaSupport then
+      "cuda-comfyui-with-extensions"
+    else if config.nixpkgs.config.rocmSupport then
+      "rocm-comfyui-with-extensions"
+    else
+      "comfyui-with-extensions";
+
+  package = nix-comfyui.packages.${pkgs.system}.${packageName};
+
   comfyui = pkgs.writeShellApplication {
     name = "comfyui";
-    runtimeInputs = [ nix-comfyui.packages.${pkgs.system}.cuda-comfyui-with-extensions ];
+    runtimeInputs = [ package ];
     text = ''
       mkdir -p "${config.xdg.dataHome}/comfyui"
       cd "${config.xdg.dataHome}/comfyui"
