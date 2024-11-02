403ebefbc376a462793978d0e9e1135a92ed98a5
haras <social@haras.anonaddy.me>
2024-11-02 19:41:35 +0100

    WIP

--- src/module/hardware/default.nix
+++ src/module/hardware/default.nix
@@ -1,14 +1,76 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 let
-  hasBluetooth =
+  memoryInBytes = (builtins.head config.facter.report.hardware.memory.resources).range;
+
+  network =
+    (builtins.hasAttr "network_controller" config.facter.report.hardware) &&
+    ((builtins.length config.facter.report.hardware.network_controller) > 0);
+
+  bluetooth =
     (builtins.hasAttr "bluetooth" config.facter.report.hardware) &&
     ((builtins.length config.facter.report.hardware.bluetooth) > 0);
 
-  hasSoundcard =
+  sound =
     (builtins.hasAttr "sound" config.facter.report.hardware) &&
     ((builtins.length config.facter.report.hardware.sound) > 0);
+
+  monitor =
+    (builtins.hasAttr "monitor" config.facter.report.hardware) &&
+    ((builtins.length config.facter.report.hardware.monitor) > 0);
+
+  graphics =
+    if
+      (builtins.hasAttr "graphics_card" config.facter.report.hardware) &&
+      ((builtins.length config.facter.report.hardware.graphics_card) > 0)
+    then
+      config.facter.report.hardware.graphics_card.driver
+    else
+      null;
+
+  keyboard =
+    (builtins.hasAttr "keyboard" config.facter.report.hardware) &&
+    ((builtins.length config.facter.report.hardware.keyboard) > 0);
+
+  mouse =
+    (builtins.hasAttr "mouse" config.facter.report.hardware) &&
+    ((builtins.length config.facter.report.hardware.mouse) > 0);
 in
 {
-  options = { };
+  options = {
+    hardware = {
+      memory = lib.mkOption {
+        type = lib.types.ints.unsigned;
+        default = memoryInBytes;
+      };
+      network = lib.mkOption {
+        type = lib.types.bool;
+        default = network;
+      };
+      bluetooth = lib.mkOption {
+        type = lib.types.bool;
+        default = bluetooth;
+      };
+      sound = lib.mkOption {
+        type = lib.types.bool;
+        default = sound;
+      };
+      monitor = lib.mkOption {
+        type = lib.types.bool;
+        default = monitor;
+      };
+      graphics = lib.mkOption {
+        type = lib.types.nullOr lib.types.enum [ "nvidia" "amd" ];
+        default = graphics;
+      };
+      keyboard = lib.mkOption {
+        type = lib.types.bool;
+        default = keyboard;
+      };
+      mouse = lib.mkOption {
+        type = lib.types.bool;
+        default = mouse;
+      };
+    };
+  };
 }
