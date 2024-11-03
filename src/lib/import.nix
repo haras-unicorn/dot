7f003c811fc08a28d0029107ad9112c3b0af183f
haras <social@haras.anonaddy.me>
2024-11-03 13:31:38 +0100

    WIP

--- src/lib/import.nix
+++ src/lib/import.nix
@@ -1,13 +1,28 @@
 { nixpkgs, ... }:
 
+let
+  importDir = importDir: dir:
+    nixpkgs.lib.filterAttrsRecursive
+      (name: value: value != null)
+      (nixpkgs.lib.attrsets.mapAttrs'
+        (name: type: {
+          name =
+            if type == "regular"
+            then (builtins.replaceStrings [ ".nix" ] [ "" ] name)
+            else name;
+          value =
+            if type == "regular"
+            then
+              if nixpkgs.lib.hasSuffix ".nix" name
+              then import "${dir}/${name}"
+              else null
+            else
+              if builtins.pathExists "${dir}/default.nix"
+              then import "${dir}/default.nix"
+              else importDir "${dir}/${name}";
+        })
+        (builtins.readDir dir));
+in
 {
-  importDir = (dir: nixpkgs.lib.attrsets.mapAttrs'
-    (name: type: {
-      name =
-        if type == "regular" then
-          (builtins.replaceStrings [ ".nix" ] [ "" ] name) else
-          name;
-      value = import "${dir}/${name}";
-    })
-    (builtins.readDir dir));
+  importDir = importDir importDir;
 }
