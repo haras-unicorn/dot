3a51864dcff2d6d85b2b2050c823e335295e2e9e
haras <social@haras.anonaddy.me>
2024-11-03 20:06:11 +0100

    WIP

--- src/module/peripherals/monitor/default.nix
+++ src/module/peripherals/monitor/default.nix
@@ -1,52 +1,24 @@
-{ lib, pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
-  options = {
+  shared = lib.mkIf config.dot.hardware.monitor.enable {
     dot = {
-      screenBrightnessDevice = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        description = "brightnessctl --list";
-        default = null;
-        example = null;
-      };
-      keyboardBrightnessDevice = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        description = "brightnessctl --list";
-        default = null;
-        example = null;
-      };
-    };
-  };
-
-  shared = {
-    dot = {
-      desktopEnvironment.keybinds = (if !(builtins.isNull config.dot.screenBrightnessDevice) then [
+      desktopEnvironment.keybinds = [
         {
           mods = [ "super" "shift" ];
           key = "b";
-          command = ''${pkgs.brightnessctl}/bin/brightnessctl --device='${config.dot.screenBrightnessDevice}' set +2%'';
+          command = ''${pkgs.brightnessctl}/bin/brightnessctl set +2%'';
         }
         {
           mods = [ "super" ];
           key = "b";
-          command = ''${pkgs.brightnessctl}/bin/brightnessctl --device='${config.dot.screenBrightnessDevice}' set 2%-'';
+          command = ''${pkgs.brightnessctl}/bin/brightnessctl set 2%-'';
         }
-      ] else [ ]) ++ (if !(builtins.isNull config.dot.keyboardBrightnessDevice) then [
-        {
-          mods = [ "super" "control" "shift" ];
-          key = "b";
-          command = ''${pkgs.brightnessctl}/bin/brightnessctl --device='${config.dot.keyboardBrightnessDevice}' set +2%'';
-        }
-        {
-          mods = [ "super" "control" ];
-          key = "b";
-          command = ''${pkgs.brightnessctl}/bin/brightnessctl --device='${config.dot.keyboardBrightnessDevice}' set 2%-'';
-        }
-      ] else [ ]);
+      ];
     };
   };
 
-  config = {
+  config = lib.mkIf config.dot.hardware.monitor.enable {
     system = {
       hardware.i2c.enable = true;
       services.ddccontrol.enable = true;
