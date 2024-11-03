6c0ffad3b37cf07a065c78621ebe5f015a396849
haras <social@haras.anonaddy.me>
2024-11-03 12:57:36 +0100

    WIP

--- src/lib/nixosConfiguration.nix
+++ src/lib/nixosConfiguration.nix
@@ -1,7 +1,34 @@
-{ self, ... }:
+{ self
+, nixpkgs
+, nur
+, nixos-facter-modules
+, sops-nix
+, home-manager
+, ...
+} @inputs:
 
-{
+let
   user = "haras";
   version = "24.11";
   modules = builtins.attrValues (self.lib.import.importDir "${self}/src/module");
+in
+{
+  inherit user version modules;
+
+  mkNixosConfiguration = system: host:
+    let
+      specialArgs = inputs // { inherit version host user; };
+    in
+    {
+      "${host}-${system}" = nixpkgs.lib.nixosSystem {
+        inherit system specialArgs;
+        modules = [
+          nur.nixosModules.nur
+          nixos-facter-modules.nixosModules.facter
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          self.nixosModules."${host}-${system}"
+        ];
+      };
+    };
 }
