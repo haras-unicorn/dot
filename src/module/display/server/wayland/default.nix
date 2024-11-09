e336de0271c61139d069ea4d6d0af4f150793c24
haras <social@haras.anonaddy.me>
2024-11-09 13:23:35 +0100

    WIP

--- src/module/display/server/wayland/default.nix
+++ src/module/display/server/wayland/default.nix
@@ -23,20 +23,34 @@ let
   hasWayland = config.dot.hardware.graphics.wayland;
 in
 {
-  system = lib.mkIf (hasMonitor && hasWayland) {
-    environment.sessionVariables = {
-      QT_QPA_PLATFORM = "wayland;xcb";
-      NIXOS_OZONE_WL = "1";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      XDG_SESSION_TYPE = "wayland";
-      MOZ_ENABLE_WAYLAND = "1";
-      NIXOS_XDG_OPEN_USE_PORTAL = "1";
-      GDK_BACKEND = "wayland,x11";
-      CLUTTER_BACKEND = "wayland";
-      SDL_VIDEODRIVER = "wayland,x11";
+  shared = lib.mkIf (hasMonitor && hasWayland) {
+    dot = {
+      desktopEnvironment.sessionVariables = {
+        QT_QPA_PLATFORM = "wayland;xcb";
+        NIXOS_OZONE_WL = "1";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        XDG_SESSION_TYPE = "wayland";
+        MOZ_ENABLE_WAYLAND = "1";
+        NIXOS_XDG_OPEN_USE_PORTAL = "1";
+        GDK_BACKEND = "wayland,x11";
+        CLUTTER_BACKEND = "wayland";
+        SDL_VIDEODRIVER = "wayland,x11";
+        _JAVA_AWT_WM_NONREPARENTING = "1";
+      };
     };
+  };
 
-    environment.systemPackages = [
+  system = lib.mkIf (hasMonitor && hasWayland) {
+    services.greetd.enable = true;
+    services.greetd.settings = {
+      default_session = {
+        command = cfg.login;
+      };
+    };
+  };
+
+  home = lib.mkIf (hasMonitor && hasWayland) {
+    home.packages = [
       pkgs.egl-wayland
       pkgs.xwaylandvideobridge
 
@@ -55,12 +69,5 @@ in
 
       pkgs.libdecor
     ];
-
-    services.greetd.enable = true;
-    services.greetd.settings = {
-      default_session = {
-        command = cfg.login;
-      };
-    };
   };
 }
