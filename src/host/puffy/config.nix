296c1bb6815382ce16bb6057e1b5247fcd88fc8b
haras <social@haras.anonaddy.me>
2024-11-10 14:50:02 +0100

    WIP

--- src/host/puffy/config.nix
+++ src/host/puffy/config.nix
@@ -1,13 +1,9 @@
 {
   shared = {
     dot = {
-      hardware = {
-        rpi."4".enable = true;
-        temp = "/sys/class/hwmon/hwmon0/temp1_input";
-      };
-      vpn = {
-        lighthouse = true;
-      };
+      hardware.rpi."4".enable = true;
+      hardware.temp = "/sys/class/hwmon/hwmon0/temp1_input";
+      vpn.lighthouse.enable = true;
     };
   };
 }
