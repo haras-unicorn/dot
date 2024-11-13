36d9d921b54106faa3eba02a8bd8d8b96ea68cbe
haras <social@haras.anonaddy.me>
2024-11-13 20:11:41 +0000

    WIP

--- src/module/user/location/default.nix
+++ src/module/user/location/default.nix
@@ -2,8 +2,12 @@
 
 {
   system = {
-    services.avahi.enable = true; # NOTE: https://github.com/NixOS/nixpkgs/issues/329522
+    # NOTE: https://github.com/NixOS/nixpkgs/issues/329522
+    services.avahi.enable = true;
     services.geoclue2.enable = true;
+    # NOTE: https://github.com/NixOS/nixpkgs/issues/293212#issuecomment-2319051915
+    services.geoclue2.geoProviderUrl = "https://beacondb.net/v1/geolocate?key=geoclue";
+
 
     location.provider = "geoclue2";
     i18n.defaultLocale = "en_US.UTF-8";
