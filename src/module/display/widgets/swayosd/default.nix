15fd8d0f74a38e3c1c0138623404bda1f8e8bc5c
haras <social@haras.anonaddy.me>
2024-11-13 20:53:05 +0100

    WIP

--- src/module/display/widgets/swayosd/default.nix
+++ src/module/display/widgets/swayosd/default.nix
@@ -1,5 +1,7 @@
 { pkgs, lib, config, ... }:
 
+# TODO: colors
+
 let
   hasMonitor = config.dot.hardware.monitor.enable;
   hasWayland = config.dot.hardware.graphics.wayland;
--- src/module/user/location/default.nix
+++ src/module/user/location/default.nix
@@ -1,11 +1,10 @@
-{ ... }:
+{ config, ... }:
 
 {
-  config = {
-    system = {
-      location.provider = "geoclue2";
-      time.timeZone = "Europe/Zagreb";
-      i18n.defaultLocale = "en_US.UTF-8";
-    };
+  system = {
+    location.provider = "geoclue2";
+    time.timeZone = "Europe/Zagreb";
+    i18n.defaultLocale = "en_US.UTF-8";
+    programs.mepo.enable = config.dot.hardware.monitor.enable;
   };
 }
