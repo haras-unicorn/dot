d578ca1ebd2246cd70149367feae84b890a392b5
haras <social@haras.anonaddy.me>
2024-11-13 09:22:11 +0100

    WIP

--- src/module/desktop/passwords/default.nix
+++ src/module/desktop/passwords/default.nix
@@ -54,9 +54,9 @@ in
           (lib.mkIf (!hasMonitor) pkgs.pinentry-curses)
         ];
 
-      # xdg.configFile."keepassxc/keepassxc.ini" = lib.mkIf (hasMonitor && hasKeyboard) {
-      #   source = ./keepassxc.ini;
-      # };
+      xdg.configFile."keepassxc/keepassxc.ini" = lib.mkIf (hasMonitor && hasKeyboard) {
+        source = ./keepassxc.ini;
+      };
     };
   };
 }
