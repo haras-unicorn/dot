e50c982a0c6312dd6985f62c701dfa57c00488e9
haras <social@haras.anonaddy.me>
2024-11-13 20:28:15 +0100

    WIP

--- src/module/display/widgets/swayosd/default.nix
+++ src/module/display/widgets/swayosd/default.nix
@@ -9,11 +9,28 @@ in
 {
   system = lib.mkIf (hasMonitor && hasWayland) {
     environment.systemPackages = [ pkgs.swayosd ];
-    # services.udev.packages = [ pkgs.swayosd ];
+    services.udev.packages = [ pkgs.swayosd ];
   };
 
   home = lib.mkIf (hasMonitor && hasWayland) {
     services.swayosd.enable = true;
     services.swayosd.display = config.dot.hardware.monitor.main;
+
+    systemd.user.services.swayosd-libinput-backend = {
+      Unit = {
+        Description = "SwayOSD LibInput backend for listening to certain keys like CapsLock, ScrollLock, VolumeUp, etc.";
+        Documentation = [ "https://github.com/ErikReider/SwayOSD" ];
+        WantedBy = [ "graphical.target" ];
+        PartOf = [ "graphical.target" ];
+        After = [ "graphical.target" ];
+      };
+
+      ServiceConfig = {
+        Type = "dbus";
+        BusName = "org.erikreider.swayosd";
+        ExecStart = "${pkgs.swayosd}/bin/swayosd-libinput-backend";
+        Restart = "on-failure";
+      };
+    };
   };
 }
