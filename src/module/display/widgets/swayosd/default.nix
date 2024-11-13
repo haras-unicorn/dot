7387fb43ffdb85c8679838f9993998cb0d8d9762
haras <social@haras.anonaddy.me>
2024-11-13 19:47:24 +0100

    WIP

--- src/module/display/widgets/swayosd/default.nix
+++ src/module/display/widgets/swayosd/default.nix
@@ -1,10 +1,15 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 
 let
   hasMonitor = config.dot.hardware.monitor.enable;
   hasWayland = config.dot.hardware.graphics.wayland;
 in
 {
+  system = lib.mkIf (hasMonitor && hasWayland) {
+    environment.systemPackages = [ pkgs.swayosd ];
+    services.udev.packages = [ pkgs.swayosd ];
+  };
+
   home = lib.mkIf (hasMonitor && hasWayland) {
     services.swayosd.enable = true;
     services.swayosd.display = config.dot.hardware.monitor.main;
