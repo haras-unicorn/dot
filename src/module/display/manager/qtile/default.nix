99be9547baf42f31c72771c6f1b04037f995c0f1
haras <social@haras.anonaddy.me>
2024-11-20 11:24:16 +0100

    WIP

--- src/module/display/manager/qtile/default.nix
+++ src/module/display/manager/qtile/default.nix
@@ -162,5 +162,12 @@ in
 
       ${windowrules}
     '';
+
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
   };
 }
