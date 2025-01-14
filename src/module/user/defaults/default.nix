e6314b25ae6afa3bd4db0cc2de145428440c6e28
haras <social@haras.anonaddy.me>
2025-01-14 14:37:54 +0100

    WIP

--- src/module/user/defaults/default.nix
+++ src/module/user/defaults/default.nix
@@ -143,6 +143,13 @@ in
     ];
   };
 
+  # NOTE: https://github.com/NixOS/nixpkgs/issues/189851#issuecomment-1759954096
+  system = {
+    systemd.user.extraConfig = ''
+      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+    '';
+  };
+
   home = {
     home.packages = [
       pkgs.xdg-user-dirs
