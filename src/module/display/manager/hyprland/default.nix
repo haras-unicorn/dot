ef42e5f33cfdcdac5f2ac129cdc0622901ce012e
haras <social@haras.anonaddy.me>
2025-01-31 14:10:35 +0100

    WIP

--- src/module/display/manager/hyprland/default.nix
+++ src/module/display/manager/hyprland/default.nix
@@ -79,7 +79,7 @@ in
     programs.hyprland.xwayland.enable = true;
     programs.hyprland.systemd.setPath.enable = true;
 
-    environeent.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".source =
+    environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".source =
       lib.mkIf hasNvidia ./50-limit-free-buffer-pool-in-wayland-compositors.json;
   };
 
