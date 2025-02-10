89f5f44c52b4714cb9bcd3d0f3a356426d828d15
haras <social@haras.anonaddy.me>
2025-02-10 07:33:43 +0100

    WIP

--- src/module/user/visual/default/default.nix
+++ src/module/user/visual/default/default.nix
@@ -75,13 +75,7 @@ in
       # csharp
       pkgs.vscode-marketplace.ms-dotnettools.vscode-dotnet-runtime
       pkgs.vscode-marketplace.selcukermaya.se-csproj-extensions
-      (pkgs.open-vsx.muhammad-sammy.csharp.overrideAttrs (final: prev: {
-        postPatch = (prev.postPatch or "") + ''
-          ls .
-          ls $out
-          mv $out/.razor $out/.razoromnisharp
-        '';
-      }))
+      pkgs.open-vsx.muhammad-sammy.csharp
       # pkgs.vscode-extensions.ms-dotnettools.csdevkit
       # pkgs.vscode-extensions.ms-dotnettools.csharp
 
