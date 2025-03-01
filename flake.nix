c756725f1b45678273833880c74eb47472f7b9c1
haras <social@haras.anonaddy.me>
2025-03-01 07:42:42 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nur.url = "github:nix-community/NUR";
