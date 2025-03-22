ae73f4b77c2581075accae40d033ff548db175a4
haras <social@haras.anonaddy.me>
2025-03-22 10:22:48 +0100

    WIP

--- src/dev.nix
+++ src/dev.nix
@@ -2,32 +2,38 @@
 
 {
   seal.defaults.devShell = "dev";
-  integrate.devShell.devShell =
-    pkgs.mkShell {
-      packages = with pkgs; [
-        # Nix
-        nil
-        nixpkgs-fmt
-
-        # Scripts
-        just
-        nushell
-        gum
-        fzf
-
-        # Misc
-        nodePackages.prettier
-        nodePackages.yaml-language-server
-        nodePackages.vscode-json-languageserver
-        marksman
-        taplo
-
-        # Tools
-        nebula
-        deploy-rs
-        vaultwarden
-        vault
-        cockroachdb
-      ];
+  integrate.devShell = {
+    nixpkgs.config = {
+      allowUnfree = true;
     };
+
+    devShell =
+      pkgs.mkShell {
+        packages = with pkgs; [
+          # Nix
+          nil
+          nixpkgs-fmt
+
+          # Scripts
+          just
+          nushell
+          gum
+          fzf
+
+          # Misc
+          nodePackages.prettier
+          nodePackages.yaml-language-server
+          nodePackages.vscode-json-languageserver
+          marksman
+          taplo
+
+          # Tools
+          nebula
+          deploy-rs
+          vaultwarden
+          vault
+          cockroachdb
+        ];
+      };
+  };
 }
