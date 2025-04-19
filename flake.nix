203ae5ebe2f60f9519d47f5d863c7f3e8ad89e62
haras <social@haras.anonaddy.me>
2025-04-19 15:34:45 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    perch.url = "github:altibiz/perch/refs/tags/2.1.1";
+    perch.url = "github:altibiz/perch/refs/tags/2.2.1";
     perch.inputs.nixpkgs.follows = "nixpkgs";
 
     rumor.url = "github:altibiz/rumor/refs/tags/1.1.3";
