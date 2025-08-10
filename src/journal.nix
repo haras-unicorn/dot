0f0c92c4545f69b0be6e2fbd06719edbdfdb1279
haras <social@haras.anonaddy.me>
2025-08-10 21:08:28 +0200

    journal

---
+++ src/journal.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  branch.nixosModule.nixosModule = {
+    services.journald.extraConfig = ''
+      SystemMaxUse=750M
+      SystemMaxFileSize=100M
+      MaxRetentionSec=1month
+    '';
+  };
+}
