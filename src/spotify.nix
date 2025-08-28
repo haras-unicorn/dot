ac3492229d1bdb6fcd12fc3758c984e34f7c5e7b
haras <social@haras.anonaddy.me>
2025-08-28 19:06:25 +0200

    spotify use psst for now at least

--- src/spotify.nix
+++ src/spotify.nix
@@ -4,11 +4,7 @@
   branch.homeManagerModule.homeManagerModule = {
     home.packages = [
       pkgs.spotify-player
+      pkgs.psst
     ];
-  };
-
-  # NOTE: https://github.com/aome510/spotify-player/issues/796#issuecomment-3172148092
-  branch.nixosModule.nixosModule = {
-    networking.extraHosts = "0.0.0.0 apresolve.spotify.com";
   };
 }
