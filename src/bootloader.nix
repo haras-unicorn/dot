8133e8a4bcde79573b00548f769f450875e1eedd
haras <social@haras.anonaddy.me>
2025-08-29 21:57:40 +0200

    separate out boot

---
+++ src/bootloader.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  ...
+}:
+
+# TODO: theming
+
+let
+  isRpi4 = config.dot.hardware.rpi."4".enable;
+in
+{
+  branch.nixosModule.nixosModule = {
+    boot.loader.efi.canTouchEfiVariables = false;
+    boot.loader.grub.enable = !isRpi4;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
+    boot.loader.grub.useOSProber = true;
+    boot.loader.generic-extlinux-compatible.enable = isRpi4;
+  };
+}
