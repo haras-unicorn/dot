ea82948c84a7419bae7138999ca999ee996d97ec
haras <social@haras.anonaddy.me>
2025-08-31 20:30:35 +0200

    firefox cleanup + search.json

--- src/firefox/default.nix
+++ src/firefox/default.nix
@@ -5,7 +5,6 @@
   ...
 }:
 
-# FIXME: duckduckgo - note that last time it was overwriting it
 # NOTE: https://github.com/arkenfox/user.js/wiki
 # NOTE: https://github.com/nix-community/nur-combined/blob/master/repos/rycee/pkgs/firefox-addons/addons.json
 # TODO: https://addons.mozilla.org/en-US/firefox/addon/textarea-cache/
@@ -13,15 +12,7 @@
 let
   hasMonitor = config.dot.hardware.monitor.enable;
 
-  fork = {
-    package = pkgs.firefox;
-    bin = "firefox";
-    stylix = "firefox";
-    stylixGnomeTheme = "firefoxGnomeTheme";
-    home = "firefox";
-  };
-
-  userjs = ''
+  userJs = ''
     ${builtins.readFile ./user.js}
 
     // set ui dark theme same as stylix
@@ -36,75 +27,122 @@ let
         ''
     }
   '';
+
+  settings = builtins.fromJSON (
+    builtins.readFile (
+      pkgs.runCommand "settings"
+        {
+          buildInputs = [
+            pkgs.nodejs
+          ];
+        }
+        ''
+          cat <<EOF | node - > $out
+          settings = {};
+          user_pref = (name, value) => settings[name] = value;
+          ${userJs}
+          console.log(JSON.stringify(settings));
+          EOF
+        ''
+    )
+  );
+
+  # NOTE: just for future reference
+  # searchJson = ''
+  #   ${builtins.readFile ./search.json}
+  # '';
+  # searchJsonMozlz4 =
+  #   pkgs.runCommand "wallpaper-image"
+  #     {
+  #       buildInputs = [
+  #         pkgs.mozlz4a
+  #       ];
+  #     }
+  #     ''
+  #       cat <<EOF | mozlz4a - $out
+  #       ${searchJson}
+  #       EOF
+  #     '';
+
+  searchAttrs = builtins.fromJSON (builtins.readFile ./search.json);
+
+  searchEngines = builtins.listToAttrs (
+    builtins.map (
+      engine:
+      let
+        fixedAttrsEngine = lib.mapAttrs' (name: value: {
+          inherit value;
+          name = builtins.replaceStrings [ "_" ] [ "" ] name;
+        }) engine;
+      in
+      {
+        name = fixedAttrsEngine.id;
+        value = fixedAttrsEngine;
+      }
+    ) searchAttrs.engines
+  );
+
+  defaultSearchEngine = searchAttrs.metaData.defaultEngineId;
+
+  extensions = [
+    pkgs.nur.repos.rycee.firefox-addons.ublock-origin
+    pkgs.nur.repos.rycee.firefox-addons.darkreader
+    pkgs.nur.repos.rycee.firefox-addons.vimium-c
+    pkgs.nur.repos.rycee.firefox-addons.i-dont-care-about-cookies
+    pkgs.nur.repos.rycee.firefox-addons.bitwarden
+    pkgs.nur.repos.rycee.firefox-addons.kagi-privacy-pass
+  ];
+
+  profileBase = {
+    settings = settings;
+    search.default = defaultSearchEngine;
+    search.engines = searchEngines;
+    search.force = true;
+    extensions.force = true;
+    extensions.packages = extensions;
+  };
 in
 {
   branch.homeManagerModule.homeManagerModule = lib.mkIf (hasMonitor) {
     dot.browser = {
-      package = fork.package;
-      bin = fork.bin;
+      package = pkgs.firefox;
+      bin = "firefox";
     };
 
-    stylix.targets.${fork.stylix} = {
-      profileNames = [ "personal" ];
-      ${fork.stylixGnomeTheme}.enable = fork.stylix != "floorp";
+    stylix.targets.firefox = {
+      profileNames = [
+        "personal"
+        "work"
+        "alternative"
+      ];
+      firefoxGnomeTheme.enable = true;
     };
 
-    programs.${fork.home} = {
+    programs.firefox = {
       enable = true;
-      package = fork.package;
       profiles = {
-        personal = {
+        personal = profileBase // {
           name = "personal";
           id = lib.mkForce 0;
           isDefault = lib.mkForce true;
-          extensions.packages = [
-            pkgs.nur.repos.rycee.firefox-addons.ublock-origin
-            pkgs.nur.repos.rycee.firefox-addons.darkreader
-            pkgs.nur.repos.rycee.firefox-addons.vimium-c
-            pkgs.nur.repos.rycee.firefox-addons.i-dont-care-about-cookies
-            pkgs.nur.repos.rycee.firefox-addons.bitwarden
-          ];
         };
-        alternative = {
+        alternative = profileBase // {
           name = "alternative";
           id = lib.mkForce 1;
           isDefault = lib.mkForce false;
-          extensions.packages = [
-            pkgs.nur.repos.rycee.firefox-addons.ublock-origin
-            pkgs.nur.repos.rycee.firefox-addons.darkreader
-            pkgs.nur.repos.rycee.firefox-addons.vimium-c
-            pkgs.nur.repos.rycee.firefox-addons.i-dont-care-about-cookies
-          ];
         };
-        work = {
+        work = profileBase // {
           name = "work";
           id = lib.mkForce 2;
           isDefault = lib.mkForce false;
-          extensions.packages = [
-            pkgs.nur.repos.rycee.firefox-addons.ublock-origin
-            pkgs.nur.repos.rycee.firefox-addons.darkreader
-            pkgs.nur.repos.rycee.firefox-addons.vimium-c
-            pkgs.nur.repos.rycee.firefox-addons.i-dont-care-about-cookies
-            pkgs.nur.repos.rycee.firefox-addons.bitwarden
-          ];
         };
       };
     };
 
-    home.file = lib.mkIf (fork.home == "firefox") {
-      ".mozilla/firefox/personal/user.js".text = userjs;
-      ".mozilla/firefox/alternative/user.js".text = userjs;
-    };
-
     xdg.desktopEntries = {
-      myfooddata = {
-        name = "My Food Data";
-        exec = "${fork.package}/bin/${fork.bin} --new-window https://myfooddata.com";
-        terminal = false;
-      };
       firefox-work = {
         name = "Firefox (work)";
-        exec = "${fork.package}/bin/${fork.bin} -P work";
+        exec = "${pkgs.firefox}/bin/firefox -P work";
         terminal = false;
       };
     };
