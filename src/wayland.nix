a69c4866b399278828bfa21db53f53f6b53d4e81
haras <social@haras.anonaddy.me>
2025-10-28 18:59:30 +0100

    fix sdl video driver

--- src/wayland.nix
+++ src/wayland.nix
@@ -20,7 +20,7 @@ in
       # NOTE: xwayland uses x11 anyway and this breaks gnome xdg portal
       # GDK_BACKEND = "wayland,x11";
       CLUTTER_BACKEND = "wayland";
-      SDL_VIDEODRIVER = "wayland,x11";
+      SDL_VIDEODRIVER = "wayland";
       _JAVA_AWT_WM_NONREPARENTING = "1";
     };
 
