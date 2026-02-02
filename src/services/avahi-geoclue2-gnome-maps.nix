f75f7216cc2ccf0e60edde816be0a31be7d3dceb
haras <social@haras.anonaddy.me>
2026-02-02 07:42:22 +0100

    swap googleapi geoclue2 for static

--- src/services/avahi-geoclue2-gnome-maps.nix
+++ src/services/avahi-geoclue2-gnome-maps.nix
@@ -10,26 +10,29 @@
     # NOTE: https://github.com/NixOS/nixpkgs/issues/329522
     services.avahi.enable = true;
     services.geoclue2.enable = true;
+    services.geoclue2.enableStatic = true;
+    # NOTE: disable the generated one from nixpkgs
+    environment.etc.geolocation.enable = lib.mkForce false;
 
     location.provider = "geoclue2";
     i18n.defaultLocale = "en_US.UTF-8";
     services.automatic-timezoned.enable = true;
 
     # NOTE: https://github.com/NixOS/nixpkgs/issues/293212#issuecomment-2319051915
-    sops.secrets."geoclue-googleapi" = {
-      path = "/etc/geoclue/conf.d/provider.conf";
+    sops.secrets."geoclue-static-geolocation" = {
+      path = "/etc/geolocation";
       owner = "geoclue";
       group = "geoclue";
-      mode = "0400";
+      mode = "0440";
     };
 
-    rumor.sops = [ "geoclue-googleapi" ];
+    rumor.sops = [ "geoclue-static-geolocation" ];
     rumor.specification.imports = [
       {
         importer = "vault-file";
         arguments = {
           path = "kv/dot/shared";
-          file = "geoclue-googleapi";
+          file = "geoclue-static-geolocation";
         };
       }
     ];
