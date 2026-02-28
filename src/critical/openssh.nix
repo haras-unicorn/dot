9189fe98b6f6707cb0ede446d6fd09d5ddb7d9d7
haras <social@haras.anonaddy.me>
2026-02-28 20:33:08 +0100

    test(critical-openssh): add e2e test with proper dot/rumor/sops mocking
    - Mock dot.host.user and dot.hardware.network.enable options
    - Import rumor module for option definitions
    - Mock sops.secrets option
    - Use lib.mkForce to override test framework defaults
    - Use machine.succeed for test assertions

--- src/critical/openssh.nix
+++ src/critical/openssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 # TODO: only allow from nebula
 
@@ -13,9 +13,9 @@
       config = lib.mkIf hasNetwork {
         services.openssh.enable = true;
         services.openssh.allowSFTP = true;
-        services.openssh.settings.PermitRootLogin = "no";
-        services.openssh.settings.PasswordAuthentication = false;
-        services.openssh.settings.KbdInteractiveAuthentication = false;
+        services.openssh.settings.PermitRootLogin = lib.mkForce "no";
+        services.openssh.settings.PasswordAuthentication = lib.mkForce false;
+        services.openssh.settings.KbdInteractiveAuthentication = lib.mkForce false;
 
         # NOTE: otherwise sops leaves .ssh owner root
         systemd.tmpfiles.rules = [
@@ -60,6 +60,50 @@
             };
           }
         ];
+      };
+    };
+
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+  perSystem =
+    { pkgs, ... }:
+    {
+      checks.test-critical-openssh = config.flake.lib.test.mkTest pkgs {
+        name = "critical-openssh";
+        nodes.machine = {
+          imports = [
+            config.flake.nixosModules.critical-openssh
+            config.flake.nixosModules.rumor
+          ];
+          options.dot.hardware.network.enable = pkgs.lib.mkOption {
+            type = pkgs.lib.types.bool;
+            default = true;
+          };
+          options.dot.host.user = pkgs.lib.mkOption {
+            type = pkgs.lib.types.str;
+            default = "testuser";
+          };
+          options.sops.secrets = pkgs.lib.mkOption {
+            type = pkgs.lib.types.attrsOf pkgs.lib.types.raw;
+            default = { };
+          };
+          config = {
+            networking.hostName = "testhost";
+            users.users.testuser = {
+              isNormalUser = true;
+              home = "/home/testuser";
+            };
+          };
+        };
+        script = ''
+          start_all()
+          machine.succeed("systemctl is-enabled sshd.service")
+          machine.succeed("grep 'PermitRootLogin no' /etc/ssh/sshd_config")
+          machine.succeed("grep 'PasswordAuthentication no' /etc/ssh/sshd_config")
+          machine.succeed("grep 'KbdInteractiveAuthentication no' /etc/ssh/sshd_config")
+        '';
       };
     };
 }
