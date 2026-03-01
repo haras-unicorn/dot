8e7cc4434c1692e707165b8a645b6b6a3bc6bc3e
haras <social@haras.anonaddy.me>
2026-03-01 12:59:47 +0100

    test(nebula): add relay test with dual-VLAN topology
    Add test for nebula relay functionality where:
    - Relay/Lighthouse is on both VLAN 1 and VLAN 2
    - Node1 is only on VLAN 1 (can reach relay on 192.168.1.1)
    - Node2 is only on VLAN 2 (can reach relay on 192.168.2.1)
    - Nodes cannot communicate directly but can reach each other through the relay
    
    Configuration includes:
    - am_relay: true on the relay node
    - use_relays: true and relays list on regular nodes
    - UDP hole punching (punchy) enabled
    - Full ping connectivity tests through the relay

--- src/critical/nebula/test.nix
+++ src/critical/nebula/test.nix
@@ -78,9 +78,7 @@
           };
         };
 
-      # Underlying network configuration - all nodes on VLAN 1
-      # Lighthouse has static IP 192.168.1.1, others use DHCP
-      underlyingNetworkIp = "192.168.1.1";
+      underlyingNetworkIp = "192.168.1.10";
     in
     {
       # Test 1: Nebula disabled when network is disabled
@@ -151,7 +149,7 @@
             config = {
               networking.hostName = "lighthouse";
               virtualisation.vlans = [ 1 ];
-              
+
               # Static IP on the underlying network (eth1)
               networking.interfaces.eth1.ipv4.addresses = [
                 {
@@ -159,7 +157,7 @@
                   prefixLength = 24;
                 }
               ];
-              
+
               dot.nebula.lighthouse = true;
               dot.nebula.ip = "10.69.42.1";
               dot.nebula.subnet.ip = "10.69.42.0";
@@ -214,7 +212,7 @@
               users.groups.nebula-dot = { };
             };
           };
-          
+
           node1 = {
             imports = [
               config.flake.nixosModules.critical-nebula
@@ -234,7 +232,7 @@
             config = {
               networking.hostName = "node1";
               virtualisation.vlans = [ 1 ];
-              
+
               dot.nebula.lighthouse = false;
               dot.nebula.ip = "10.69.42.2";
               dot.nebula.subnet.ip = "10.69.42.0";
@@ -292,7 +290,7 @@
               users.groups.nebula-dot = { };
             };
           };
-          
+
           node2 = {
             imports = [
               config.flake.nixosModules.critical-nebula
@@ -312,7 +310,7 @@
             config = {
               networking.hostName = "node2";
               virtualisation.vlans = [ 1 ];
-              
+
               dot.nebula.lighthouse = false;
               dot.nebula.ip = "10.69.42.3";
               dot.nebula.subnet.ip = "10.69.42.0";
@@ -425,19 +423,386 @@
           # Test connectivity through nebula
           # node1 -> lighthouse
           node1.succeed("ping -c 3 10.69.42.1")
-          
+
           # node2 -> lighthouse
           node2.succeed("ping -c 3 10.69.42.1")
-          
+
           # lighthouse -> node1
           lighthouse.succeed("ping -c 3 10.69.42.2")
-          
+
           # lighthouse -> node2
           lighthouse.succeed("ping -c 3 10.69.42.3")
-          
+
           # node1 <-> node2 (mesh connectivity)
           node1.succeed("ping -c 3 10.69.42.3")
           node2.succeed("ping -c 3 10.69.42.2")
+        '';
+      };
+
+      # Test 3: Nebula relay test - nodes on different VLANs that can only communicate through the relay
+      checks.test-critical-nebula-relay = config.flake.lib.test.mkTest pkgs {
+        name = "critical-nebula-relay";
+        nodes = {
+          # Relay/Lighthouse - connected to both VLANs
+          relay = {
+            imports = [
+              config.flake.nixosModules.critical-nebula
+              config.flake.nixosModules.rumor
+              mockChronydModule
+            ];
+            options = pkgs.lib.recursiveUpdate nebulaOptions {
+              dot.host.user = pkgs.lib.mkOption {
+                type = pkgs.lib.types.str;
+                default = "testuser";
+              };
+              sops.secrets = pkgs.lib.mkOption {
+                type = pkgs.lib.types.attrsOf sopsSecretsSubmodule;
+                default = { };
+              };
+            };
+            config = {
+              networking.hostName = "relay";
+              # Relay is on both VLANs
+              virtualisation.vlans = [
+                1
+                2
+              ];
+
+              networking.interfaces.eth1.ipv4.addresses = [
+                {
+                  address = "192.168.1.10";
+                  prefixLength = 24;
+                }
+              ];
+              networking.interfaces.eth2.ipv4.addresses = [
+                {
+                  address = "192.168.2.10";
+                  prefixLength = 24;
+                }
+              ];
+
+              dot.nebula.lighthouse = true;
+              dot.nebula.ip = "10.69.42.1";
+              dot.nebula.subnet.ip = "10.69.42.0";
+              dot.nebula.subnet.bits = 24;
+              dot.nebula.subnet.mask = "255.255.255.0";
+
+              # Set up mock sops secrets pointing to test certs
+              sops.secrets = {
+                "nebula-ca-public" = {
+                  path = "${testCerts}/ca.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-public" = {
+                  path = "${testCerts}/lighthouse.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-private" = {
+                  path = "${testCerts}/lighthouse.key";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+                "nebula-lighthouse" = {
+                  path = "/etc/nebula/config.d/lighthouse.yaml";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+              };
+
+              # Relay configuration with am_relay: true
+              environment.etc."nebula/config.d/lighthouse.yaml".text = ''
+                lighthouse:
+                  am_lighthouse: true
+                  serve_dns: false
+                  interval: 60
+                punchy:
+                  punch: true
+                relay:
+                  am_relay: true
+                  use_relays: true
+              '';
+
+              users.users.testuser = {
+                isNormalUser = true;
+                home = "/home/testuser";
+              };
+
+              users.users.nebula-dot = {
+                isSystemUser = true;
+                group = "nebula-dot";
+              };
+              users.groups.nebula-dot = { };
+            };
+          };
+
+          # Node1 - only on VLAN 1 (can reach relay on 192.168.1.10)
+          node1 = {
+            imports = [
+              config.flake.nixosModules.critical-nebula
+              config.flake.nixosModules.rumor
+              mockChronydModule
+            ];
+            options = pkgs.lib.recursiveUpdate nebulaOptions {
+              dot.host.user = pkgs.lib.mkOption {
+                type = pkgs.lib.types.str;
+                default = "testuser";
+              };
+              sops.secrets = pkgs.lib.mkOption {
+                type = pkgs.lib.types.attrsOf sopsSecretsSubmodule;
+                default = { };
+              };
+            };
+            config = {
+              networking.hostName = "node1";
+              virtualisation.vlans = [ 1 ];
+
+              networking.interfaces.eth1.ipv4.addresses = [
+                {
+                  address = "192.168.1.20";
+                  prefixLength = 24;
+                }
+              ];
+
+              dot.nebula.lighthouse = false;
+              dot.nebula.ip = "10.69.42.2";
+              dot.nebula.subnet.ip = "10.69.42.0";
+              dot.nebula.subnet.bits = 24;
+              dot.nebula.subnet.mask = "255.255.255.0";
+
+              # Set up mock sops secrets pointing to test certs
+              sops.secrets = {
+                "nebula-ca-public" = {
+                  path = "${testCerts}/ca.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-public" = {
+                  path = "${testCerts}/node1.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-private" = {
+                  path = "${testCerts}/node1.key";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+                "nebula-lighthouse" = {
+                  path = "/etc/nebula/config.d/lighthouse.yaml";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+              };
+
+              # Node configuration with relay support
+              environment.etc."nebula/config.d/lighthouse.yaml".text = ''
+                static_host_map:
+                  "10.69.42.1": ["192.168.1.10:4242"]
+                lighthouse:
+                  am_lighthouse: false
+                  hosts:
+                    - '10.69.42.1'
+                  interval: 60
+                punchy:
+                  punch: true
+                relay:
+                  am_relay: false
+                  use_relays: true
+                  relays:
+                    - "10.69.42.1"
+              '';
+
+              users.users.testuser = {
+                isNormalUser = true;
+                home = "/home/testuser";
+              };
+
+              users.users.nebula-dot = {
+                isSystemUser = true;
+                group = "nebula-dot";
+              };
+              users.groups.nebula-dot = { };
+            };
+          };
+
+          # Node2 - only on VLAN 2 (can reach relay on 192.168.2.10)
+          node2 = {
+            imports = [
+              config.flake.nixosModules.critical-nebula
+              config.flake.nixosModules.rumor
+              mockChronydModule
+            ];
+            options = pkgs.lib.recursiveUpdate nebulaOptions {
+              dot.host.user = pkgs.lib.mkOption {
+                type = pkgs.lib.types.str;
+                default = "testuser";
+              };
+              sops.secrets = pkgs.lib.mkOption {
+                type = pkgs.lib.types.attrsOf sopsSecretsSubmodule;
+                default = { };
+              };
+            };
+            config = {
+              networking.hostName = "node2";
+              virtualisation.vlans = [ 2 ];
+
+              networking.interfaces.eth1.ipv4.addresses = [
+                {
+                  address = "192.168.2.20";
+                  prefixLength = 24;
+                }
+              ];
+
+              dot.nebula.lighthouse = false;
+              dot.nebula.ip = "10.69.42.3";
+              dot.nebula.subnet.ip = "10.69.42.0";
+              dot.nebula.subnet.bits = 24;
+              dot.nebula.subnet.mask = "255.255.255.0";
+
+              # Set up mock sops secrets pointing to test certs
+              sops.secrets = {
+                "nebula-ca-public" = {
+                  path = "${testCerts}/ca.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-public" = {
+                  path = "${testCerts}/node2.crt";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0644";
+                };
+                "nebula-private" = {
+                  path = "${testCerts}/node2.key";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+                "nebula-lighthouse" = {
+                  path = "/etc/nebula/config.d/lighthouse.yaml";
+                  owner = "nebula-dot";
+                  group = "nebula-dot";
+                  mode = "0400";
+                };
+              };
+
+              # Node configuration with relay support
+              environment.etc."nebula/config.d/lighthouse.yaml".text = ''
+                static_host_map:
+                  "10.69.42.1": ["192.168.2.10:4242"]
+                lighthouse:
+                  am_lighthouse: false
+                  hosts:
+                    - '10.69.42.1'
+                  interval: 60
+                punchy:
+                  punch: true
+                relay:
+                  am_relay: false
+                  use_relays: true
+                  relays:
+                    - "10.69.42.1"
+              '';
+
+              users.users.testuser = {
+                isNormalUser = true;
+                home = "/home/testuser";
+              };
+
+              users.users.nebula-dot = {
+                isSystemUser = true;
+                group = "nebula-dot";
+              };
+              users.groups.nebula-dot = { };
+            };
+          };
+        };
+
+        script = ''
+          import time
+
+          # Start all VMs
+          start_all()
+
+          # Wait for network to be online
+          relay.wait_for_unit("network-online.target")
+          node1.wait_for_unit("network-online.target")
+          node2.wait_for_unit("network-online.target")
+
+          # Wait for nebula service to start on all nodes
+          relay.wait_for_unit("nebula@dot.service")
+          node1.wait_for_unit("nebula@dot.service")
+          node2.wait_for_unit("nebula@dot.service")
+
+          # Wait for nebula TUN interface to be up
+          relay.wait_until_succeeds("ip link show nebula-dot")
+          node1.wait_until_succeeds("ip link show nebula-dot")
+          node2.wait_until_succeeds("ip link show nebula-dot")
+
+          # Give nebula time to establish connections via relay
+          # This takes longer because nodes need to discover they can't reach each other directly
+          time.sleep(15)
+
+          # Verify config files exist and have relay configuration
+          # Relay config check
+          relay.succeed("test -f /etc/nebula/config.d/config.yaml")
+          relay.succeed("grep -q 'am_relay: true' /etc/nebula/config.d/lighthouse.yaml")
+
+          # Node config checks
+          node1.succeed("test -f /etc/nebula/config.d/config.yaml")
+          node1.succeed("test -f /etc/nebula/config.d/lighthouse.yaml")
+          node1.succeed("grep -q 'use_relays: true' /etc/nebula/config.d/lighthouse.yaml")
+          node1.succeed("grep -q '10.69.42.1' /etc/nebula/config.d/lighthouse.yaml")
+
+          node2.succeed("test -f /etc/nebula/config.d/config.yaml")
+          node2.succeed("test -f /etc/nebula/config.d/lighthouse.yaml")
+          node2.succeed("grep -q 'use_relays: true' /etc/nebula/config.d/lighthouse.yaml")
+          node2.succeed("grep -q '10.69.42.1' /etc/nebula/config.d/lighthouse.yaml")
+
+          # Verify firewall: relay should have port 4242 open
+          relay.succeed("iptables -L -n | grep -q '4242'")
+
+          # Verify firewall: nodes should NOT have port 4242 open
+          node1.fail("iptables -L -n | grep -q '4242'")
+          node2.fail("iptables -L -n | grep -q '4242'")
+
+          # Verify nebula interface has the correct IP
+          relay.succeed("ip addr show nebula-dot | grep -q '10.69.42.1'")
+          node1.succeed("ip addr show nebula-dot | grep -q '10.69.42.2'")
+          node2.succeed("ip addr show nebula-dot | grep -q '10.69.42.3'")
+
+          # Test connectivity through underlying network
+          # Each node can reach the relay
+          node1.succeed("ping -c 3 192.168.1.10")
+          node2.succeed("ping -c 3 192.168.2.10")
+          relay.succeed("ping -c 3 192.168.1.20")
+          relay.succeed("ping -c 3 192.168.2.20")
+
+          # Test connectivity through relay
+          # Each node can reach the relay
+          node1.succeed("ping -c 3 10.69.42.1")
+          node2.succeed("ping -c 3 10.69.42.1")
+          relay.succeed("ping -c 3 10.69.42.2")
+          relay.succeed("ping -c 3 10.69.42.3")
+
+          # THE KEY TEST: nodes can reach each other THROUGH THE RELAY
+          # even though they are on different VLANs and can't communicate directly
+          node1.succeed("ping -c 3 10.69.42.3")
+          node2.succeed("ping -c 3 10.69.42.2")
+
+          # Verify relay is actually being used by checking that nodes can't reach each other directly
+          # Node1 should not be able to ping node2's underlying IP
+          node1.fail("ping -c 1 192.168.2.10 || ping -c 1 192.168.2.20")
         '';
       };
     };
