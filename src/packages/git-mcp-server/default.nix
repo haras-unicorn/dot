893bee368baf02be55a1d538e3aeaaeac1378db2
haras-unicorn <github@haras-unicorn.net>
2026-08-11 23:00:02 +0200

    wip

--- src/packages/git-mcp-server/default.nix
+++ src/packages/git-mcp-server/default.nix
@@ -5,13 +5,15 @@
       system = pkgs.stdenv.hostPlatform.system;
 
       # NOTE: version 2.1.2
-      bun2nix = builtins.getFlake (
+      bun2nixFlake = builtins.getFlake (
         "github:nix-community/bun2nix/0f2a1f0b6f42cebe3b149bf62d38754c5e0e9729"
         + "?narHash=sha256-9BMxlTxCCDAeoNLtb1a/st7udtTIJep%2BwpUzquA29VU%3D"
       );
+
+      bun2nix = bun2nixFlake.packages.${system}.default;
     in
     {
-      packages.git-mcp-server = bun2nix.packages.${system}.default.mkDerivation (final: {
+      packages.git-mcp-server = bun2nix.mkDerivation (final: {
         pname = "git-mcp-server";
         version = "2.15.1";
 
@@ -26,7 +28,7 @@
           bunNix = ./bun.nix.lock;
         };
 
-        module = "index.ts";
+        module = "src/index.ts";
       });
     };
 }
