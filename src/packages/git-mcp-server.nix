83825407fd7a2ca15a5b907b587daf265e618fa2
haras-unicorn <github@haras-unicorn.net>
2026-08-11 22:49:48 +0200

    wip

--- src/packages/git-mcp-server.nix
+++ src/packages/git-mcp-server.nix
@@ -2,8 +2,9 @@
   perSystem =
     { lib, pkgs, ... }:
     let
+      # NOTE: version 2.1.2
       bun2nix = builtins.getFlake (
-        "github:nix-community/bun2nix/refs/tags/2.1.2"
+        "github:nix-community/bun2nix/0f2a1f0b6f42cebe3b149bf62d38754c5e0e9729"
         + "?narHash=sha256-9BMxlTxCCDAeoNLtb1a/st7udtTIJep%2BwpUzquA29VU%3D"
       );
     in
