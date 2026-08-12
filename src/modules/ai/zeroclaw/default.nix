78d98ea974226fb7472f9849f3a755f8e82c79c8
haras-unicorn <github@haras-unicorn.net>
2026-08-12 11:03:15 +0200

    wip

--- src/modules/ai/zeroclaw/default.nix
+++ src/modules/ai/zeroclaw/default.nix
@@ -23,7 +23,7 @@
         runtimeInputs = [ zeroclaw ];
         text = ''
           export ZEROCLAW_CONFIG_DIR="${dataDir}"
-          /run/wrappers/bin/sudo -u "${user}" "$@"
+          /run/wrappers/bin/sudo -u "${user}" zeroclaw "$@"
         '';
       };
 
