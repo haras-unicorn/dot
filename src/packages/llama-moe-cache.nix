2ba767b7d9ec87cf58b1a38d5f6311e37b0a3907
haras-unicorn <github@haras-unicorn.net>
2026-08-14 11:02:02 +0200

    wip

--- src/packages/llama-moe-cache.nix
+++ src/packages/llama-moe-cache.nix
@@ -1,54 +1,20 @@
-{ inputs, ... }:
-
 {
   perSystem =
     { pkgs, ... }:
-    let
-      system = pkgs.stdenv.hostPlatform.system;
-
-      makePackage =
-        {
-          config ? { },
-          override ? { },
-        }:
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-            }
-            // config;
-          };
-
-          src = pkgs.fetchFromGitHub {
-            owner = "ongunm";
-            repo = "llama-moe-cache";
-            rev = "77c8767d26bd6285b2fe351c58143ee4d6b72fa6";
-            hash = "sha256-5sAd5aSmC926ND1IZY5FtkAjRcJCvSzlJHTXJk+jjj8=";
-          };
-
-          scope = pkgs.callPackage "${src}/.devops/nix/scope.nix" { };
-        in
-        scope.llama-cpp.override override;
-    in
     {
-      packages = {
-        llama-moe-cache = makePackage { };
-        llama-moe-cache-cuda = makePackage {
-          config = {
-            cudaSupport = true;
-          };
+      packages.llama-moe-cache-cuda = (pkgs.llama-cpp.override { cudaSupport = true; }).overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "haras-unicorn";
+          repo = "llama.cpp";
+          rev = "d7e9b46b3c052be760f4d2786a0fa16e8e7fb7a4";
+          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          leaveDotGit = true;
+          postFetch = ''
+            git -C "$out" rev-parse --short HEAD > $out/COMMIT
+            find "$out" -name .git -print0 | xargs -0 rm -rf
+          '';
         };
-        llama-moe-cache-rocm = makePackage {
-          config = {
-            rocmSupport = true;
-          };
-        };
-        llama-moe-cache-vulkan = makePackage {
-          override = {
-            useVulkan = true;
-          };
-        };
+        npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
       };
     };
 }
