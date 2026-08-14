be03751bb1b956ffaea4d277227aa0216fdf1d54
haras-unicorn <github@haras-unicorn.net>
2026-08-14 11:54:17 +0200

    wip

--- src/packages/llama-moe-cache.nix
+++ src/packages/llama-moe-cache.nix
@@ -8,7 +8,7 @@
           owner = "haras-unicorn";
           repo = "llama.cpp";
           rev = "03c4325d236f1c8cd91949ea330468bf8ff7e6b7";
-          hash = "sha256-zyu4oWrrrHSbvzfRZOgbx1YCGd5X3Lp6rMUfIDVUtfU=";
+          hash = "sha256-P7XYSrMdLF4asEpw3AiDWix4rmvWLpIWh4fQaOEmqLM=";
           leaveDotGit = true;
           postFetch = ''
             git -C "$out" rev-parse --short HEAD > $out/COMMIT
